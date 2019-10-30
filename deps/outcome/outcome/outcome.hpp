c3e5239653573355fa7de196766a0bc3efc8ed29
kamilsa <kamilsa16@gmail.com>
2019-10-30 21:55:20 +0300

    Add kagome commons
    Signed-off-by: kamilsa <kamilsa16@gmail.com>

---
+++ deps/outcome/outcome/outcome.hpp
@@ -0,0 +1,17 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CPP_FILECOIN_DEPS_OUTCOME_OUTCOME_OUTCOME_HPP
+#define CPP_FILECOIN_DEPS_OUTCOME_OUTCOME_OUTCOME_HPP
+
+#include <libp2p/outcome/outcome.hpp>
+
+namespace outcome {
+  using libp2p::outcome::failure;
+  using libp2p::outcome::result;
+  using libp2p::outcome::success;
+}  // namespace outcome
+
+#endif  // CPP_FILECOIN_DEPS_OUTCOME_OUTCOME_OUTCOME_HPP
---
+++ test/testutil/literals.hpp
@@ -0,0 +1,16 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CPP_FILECOIN_TEST_TESTUTIL_LITERALS_HPP
+#define CPP_FILECOIN_TEST_TESTUTIL_LITERALS_HPP
+
+#include "common/blob.hpp"
+#include "common/hexutil.hpp"
+
+inline std::vector<uint8_t> operator""_unhex(const char *c, size_t s) {
+  return filecoin::common::unhex(std::string_view(c, s)).value();
+}
+
+#endif  // CPP_FILECOIN_TEST_TESTUTIL_LITERALS_HPP
