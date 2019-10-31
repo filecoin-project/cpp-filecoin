6bf85bddb1692501078dc85122d2d45a08896a5b
masterjedy <mizuki@mizukis-MBP.Dlink>
2019-10-31 16:48:02 +0300

    fix deps
    Signed-off-by: masterjedy <yura.zarudny@gmail.com>

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -7,12 +7,23 @@ find_package(GTest CONFIG REQUIRED)
 find_package(GMock CONFIG REQUIRED)
 
 # https://docs.hunter.sh/en/latest/packages/pkg/Boost.html
-hunter_add_package(Boost)
-find_package(Boost CONFIG REQUIRED)
+hunter_add_package(Boost COMPONENTS filesystem random)
+find_package(Boost CONFIG REQUIRED filesystem random)
 
 # https://docs.hunter.sh/en/latest/packages/pkg/Microsoft.GSL.html
 hunter_add_package(Microsoft.GSL)
 find_package(Microsoft.GSL CONFIG REQUIRED)
 
+# https://www.openssl.org/
+hunter_add_package(OpenSSL)
+find_package(OpenSSL REQUIRED)
+
+# https://developers.google.com/protocol-buffers/
+hunter_add_package(Protobuf)
+find_package(Protobuf CONFIG REQUIRED)
+
+hunter_add_package(spdlog)
+find_package(spdlog CONFIG REQUIRED)
+
 hunter_add_package(libp2p)
-find_package(libp2p REQUIRED)
+find_package(libp2p CONFIG REQUIRED)
