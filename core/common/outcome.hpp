f6fbaa7787a04cd2f92eb5b97045819fd260650b
Harrm <abrehchs@gmail.com>
2019-11-05 17:27:03 +0300

    Fix outcome target to be valid for export

--- core/common/outcome.hpp
+++ core/common/outcome.hpp
@@ -3,8 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef CPP_FILECOIN_DEPS_OUTCOME_OUTCOME_OUTCOME_HPP
-#define CPP_FILECOIN_DEPS_OUTCOME_OUTCOME_OUTCOME_HPP
+#ifndef CPP_FILECOIN_CORE_COMMON_OUTCOME_HPP
+#define CPP_FILECOIN_CORE_COMMON_OUTCOME_HPP
 
 #include <libp2p/outcome/outcome.hpp>
 
@@ -14,4 +14,4 @@ namespace outcome {
   using libp2p::outcome::success;
 }  // namespace outcome
 
-#endif  // CPP_FILECOIN_DEPS_OUTCOME_OUTCOME_OUTCOME_HPP
+#endif // CPP_FILECOIN_CORE_COMMON_OUTCOME_HPP
