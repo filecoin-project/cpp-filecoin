4d73dd5589347e33a7c5846bee049a6003a9a5d9
Alexey <Alexey.N.Chernyshov@gmail.com>
2019-12-12 12:04:59 +0300

    Handy wrapper for blake2b-160 hash (#24)
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>

---
+++ core/crypto/blake2/blake2b160.hpp
@@ -0,0 +1,31 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CPP_FILECOIN_BLAKE2B160_HPP
+#define CPP_FILECOIN_BLAKE2B160_HPP
+
+#include <gsl/span>
+
+#include "common/blob.hpp"
+#include "common/outcome.hpp"
+#include "crypto/blake2/blake2b_error.hpp"
+
+namespace fc::crypto::blake2b {
+
+  const size_t BLAKE2B160_HASH_LENGHT = 20;  // 160 BIT
+
+  using Blake2b160Hash = common::Blob<BLAKE2B160_HASH_LENGHT>;
+
+  /**
+   * @brief Get blake2b-160 hash
+   * @param to_hash - data to hash
+   * @return outcome with hash or error
+   */
+  fc::outcome::result<Blake2b160Hash> blake2b_160(
+      const gsl::span<uint8_t> &to_hash);
+
+}  // namespace fc::crypto::blake2b
+
+#endif  // CPP_FILECOIN_BLAKE2B160_HPP
