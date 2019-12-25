cd1d23fddbbb99a53f7cbaecdfd23a3a5f9a6142
turuslan <turuslan.devbox@gmail.com>
2019-12-25 20:35:13 +0300

    testutil cbor
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

---
+++ test/testutil/cbor.hpp
@@ -0,0 +1,27 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CPP_FILECOIN_TEST_TESTUTIL_CBOR_HPP
+#define CPP_FILECOIN_TEST_TESTUTIL_CBOR_HPP
+
+#include "codec/cbor/cbor.hpp"
+#include "testutil/literals.hpp"
+#include "testutil/outcome.hpp"
+
+template <typename T>
+void expectEncodeAndReencode(const T &value,
+                             const std::vector<uint8_t> &bytes) {
+  EXPECT_OUTCOME_EQ(fc::codec::cbor::encode(value), bytes);
+  EXPECT_OUTCOME_TRUE(decoded, fc::codec::cbor::decode<T>(bytes));
+  EXPECT_OUTCOME_EQ(fc::codec::cbor::encode(decoded), bytes);
+}
+
+inline auto operator""_cid(const char *c, size_t s) {
+  return libp2p::multi::ContentIdentifierCodec::decode(
+             fc::common::unhex(std::string_view(c, s)).value())
+      .value();
+}
+
+#endif  // CPP_FILECOIN_TEST_TESTUTIL_CBOR_HPP
