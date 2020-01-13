8467df52b00e9822e8ad470c0184eb7fe57cb258
Yura Zarudniy <zarudniy@soramitsu.co.jp>
2020-01-13 20:47:06 +0300

    add tests for ticket codec
    Signed-off-by: Yura Zarudniy <zarudniy@soramitsu.co.jp>

--- test/core/primitives/ticket/ticket_codec_test.cpp
+++ test/core/primitives/ticket/ticket_codec_test.cpp
@@ -6,15 +6,50 @@
 #include "primitives/ticket/ticket_codec.hpp"
 
 #include <gtest/gtest.h>
-#include "testutil/primitives/ticket_utils.hpp"
 #include "testutil/outcome.hpp"
+#include "testutil/primitives/ticket_utils.hpp"
 
- struct TicketCodecTest: public ::testing::Test {
-   using Ticket = fc::primitives::ticket::Ticket;
+using fc::codec::cbor::CborDecodeStream;
+using fc::codec::cbor::CborEncodeStream;
+using fc::common::Buffer;
+using fc::crypto::vrf::VRFHash;
+using fc::crypto::vrf::VRFProof;
+using fc::primitives::ticket::Ticket;
 
-   TicketGenerator generator;
- };
+struct TicketCodecTest : public ::testing::Test {
+  using Ticket = fc::primitives::ticket::Ticket;
 
- TEST_F(TicketCodecTest, EncodeDecodeSuccess) {
+  TicketGenerator generator{};
+};
 
- }
+TEST_F(TicketCodecTest, EncodeDecodeSuccess) {
+  auto &&t1 = generator.makeTicket();
+  CborEncodeStream es{};
+  ASSERT_NO_THROW(es << t1);
+  CborDecodeStream ds{es.data()};
+  Ticket t2{};
+  ASSERT_NO_THROW(ds >> t2);
+  ASSERT_EQ(t1, t2);
+}
+
+TEST_F(TicketCodecTest, DecodeFromLotusSuccess) {
+  auto hex_ticket =
+      fc::common::Blob<96>::fromHex(
+          "01010101010101010101010101010101010101010101010101010101010101010101"
+          "01010101010101010101010101010101010101010101010101010101010101010101"
+          "01010101010101010101010101010101010101010101010101010101")
+          .value();
+  auto ticket = Ticket{VRFProof(hex_ticket)};
+
+  std::string cbor_value =
+      "815860010101010101010101010101010101010101010101010101010101010101010101"
+      "010101010101010101010101010101010101010101010101010101010101010101010101"
+      "010101010101010101010101010101010101010101010101010101";
+
+  EXPECT_OUTCOME_TRUE(cbor_data, Buffer::fromHex(cbor_value));
+
+  CborDecodeStream ds{cbor_data};
+  Ticket ticket1{};
+  ASSERT_NO_THROW(ds >> ticket1);
+  ASSERT_EQ(ticket1, ticket);
+}
