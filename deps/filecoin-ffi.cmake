9d5b05a02a7f56aaf2b9136f9800cd7ecb738772
artyom-yurin <artem_yrin@mail.ru>
2020-02-06 03:04:46 +0300

    Fixed cmake
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

--- deps/filecoin-ffi.cmake
+++ deps/filecoin-ffi.cmake
@@ -11,17 +11,35 @@ set(FILECOIN_FFI_LIB
         "${FILECOIN_FFI_PATH}/libfilecoin.a")
 
 file(MAKE_DIRECTORY ${FILECOIN_FFI_INCLUDES})
-set(ENV{FFI_BUILD_FROM_SOURCE} "1")
+
+set(ENV{PKG_CONFIG_PATH}  "${PKG_CONFIG_PATH}:${FILECOIN_FFI_PATH}")
+
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(PKG_FILECOIN filecoin)
+
+if (NOT PKG_FILECOIN_FOUND)
+    message("Installing filecoin-ffi")
+    execute_process(COMMAND make clean all
+            WORKING_DIRECTORY ${FILECOIN_FFI_PATH})
+
+    execute_process(COMMAND mkdir -p ${FILECOIN_FFI_INCLUDES}
+            COMMAND cp "${FILECOIN_FFI_PATH}/filecoin.h" ${FILECOIN_FFI_INCLUDES}
+            WORKING_DIRECTORY ${FILECOIN_FFI_PATH})
+
+    pkg_check_modules(PKG_FILECOIN REQUIRED filecoin)
+endif (NOT PKG_FILECOIN_FOUND)
+
+
 
 add_custom_target(
         filecoin_ffi_build
         COMMAND make
         WORKING_DIRECTORY ${FILECOIN_FFI_PATH}
 )
-
 add_custom_target(
         filecoin_ffi_fix_include
-        COMMAND mkdir -p ${FILECOIN_FFI_INCLUDES} && cp "${FILECOIN_FFI_PATH}/filecoin.h" ${FILECOIN_FFI_INCLUDES}
+        COMMAND mkdir -p ${FILECOIN_FFI_INCLUDES}
+        COMMAND cp "${FILECOIN_FFI_PATH}/filecoin.h" ${FILECOIN_FFI_INCLUDES}
         WORKING_DIRECTORY ${FILECOIN_FFI_PATH}
 )
 
@@ -32,11 +50,6 @@ add_dependencies(filecoin_ffi_fix_include
 add_library(filecoin_ffi
         STATIC IMPORTED GLOBAL
         )
-
-set(ENV{PKG_CONFIG_PATH}  "${PKG_CONFIG_PATH}:${FILECOIN_FFI_PATH}")
-
-find_package(PkgConfig REQUIRED)
-pkg_check_modules(PKG_FILECOIN REQUIRED filecoin)
 
 target_link_libraries(filecoin_ffi INTERFACE ${PKG_FILECOIN_LIBRARIES})
 target_include_directories(filecoin_ffi PUBLIC ${PKG_FILECOIN_INCLUDE_DIRS})
