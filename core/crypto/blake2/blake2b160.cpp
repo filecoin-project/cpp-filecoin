1a0609193b67bcabb38f1416929be5363f90f404
artyom-yurin <artem_yrin@mail.ru>
2020-02-13 13:58:44 +0300

    Rewrote blake2b from file to our implementation
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

--- core/crypto/blake2/blake2b160.cpp
+++ core/crypto/blake2/blake2b160.cpp
@@ -5,7 +5,6 @@
 
 #include "crypto/blake2/blake2b160.hpp"
 
-#include <openssl/evp.h>
 #include <iostream>
 #include "crypto/blake2/blake2b.h"
 
@@ -36,23 +35,22 @@ namespace fc::crypto::blake2b {
   Blake2b512Hash blake2b_512_from_file(std::ifstream &file_stream) {
     if (!file_stream.is_open()) return {};
 
-    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
-    const EVP_MD *blake2b = EVP_blake2b512();
-    EVP_DigestInit_ex(mdctx, blake2b, NULL);
+    blake2b_ctx ctx;
+
+    if (blake2b_init(&ctx, BLAKE2B512_HASH_LENGTH, nullptr, 0)) {
+      return {};
+    }
 
     const int one_kb = 1024;
     std::string bytes(one_kb, ' ');
     int bytes_count = file_stream.readsome(bytes.data(), one_kb);
     while (bytes_count != 0) {
-      EVP_DigestUpdate(mdctx, bytes.data(), bytes_count);
+      blake2b_update(&ctx, bytes.data(), bytes_count);
       bytes_count = file_stream.readsome(bytes.data(), one_kb);
     }
 
     Blake2b512Hash hash;
-    unsigned int size = BLAKE2B512_HASH_LENGTH;
-    EVP_DigestFinal_ex(mdctx, hash.data(), &size);
-
-    EVP_MD_CTX_free(mdctx);
+    blake2b_final(&ctx, hash.data());
     return hash;
   }
 
