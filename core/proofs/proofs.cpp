84a1113f11a93596e84b7acfadc22d0f52a97b9e
artyom-yurin <artem_yrin@mail.ru>
2020-02-27 21:30:04 +0300

    Cleaned up comments
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

--- core/proofs/proofs.cpp
+++ core/proofs/proofs.cpp
@@ -56,35 +56,6 @@ namespace fc::proofs {
     return cpp_candidates;
   }
 
-  /*
-
-  std::vector<Candidate> cppCandidates(
-      const gsl::span<const FFICandidate> &c_candidates) {
-    std::vector<Candidate> cpp_candidates;
-
-    for (const auto &c_candidate : c_candidates) {
-      cpp_candidates.push_back(cppCandidate(c_candidate));
-    }
-
-    return cpp_candidates;
-  }
-
-  RawSealPreCommitOutput cppRawSealPreCommitOutput(
-      const FFISealPreCommitOutput &c_seal_pre_commit_output) {
-    RawSealPreCommitOutput cpp_seal_pre_commit_output;
-
-    std::copy(c_seal_pre_commit_output.comm_d,
-              // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-              c_seal_pre_commit_output.comm_d + kCommitmentBytesLen,
-              cpp_seal_pre_commit_output.comm_d.begin());
-    std::copy(c_seal_pre_commit_output.comm_r,
-              // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-              c_seal_pre_commit_output.comm_r + kCommitmentBytesLen,
-              cpp_seal_pre_commit_output.comm_r.begin());
-
-    return cpp_seal_pre_commit_output;
-  }*/
-
   WriteWithoutAlignmentResult cppWriteWithoutAlignmentResult(
       const WriteWithoutAlignmentResponse &response) {
     WriteWithoutAlignmentResult result;
@@ -152,28 +123,6 @@ namespace fc::proofs {
     // NOLINTNEXTLINE
     return reinterpret_cast<const uint8_t(*)[32]>(arr.data());
   }
-
-  /*FFISealPreCommitOutput cRawSealPreCommitOutput(
-      const RawSealPreCommitOutput &cpp_seal_pre_commit_output) {
-    FFISealPreCommitOutput c_seal_pre_commit_output{};
-
-    std::copy(cpp_seal_pre_commit_output.comm_d.begin(),
-              cpp_seal_pre_commit_output.comm_d.end(),
-              c_seal_pre_commit_output.comm_d);
-    std::copy(cpp_seal_pre_commit_output.comm_r.begin(),
-              cpp_seal_pre_commit_output.comm_r.end(),
-              c_seal_pre_commit_output.comm_r);
-
-    return c_seal_pre_commit_output;
-  }
-
-  FFISectorClass cSectorClass(const uint64_t sector_size,
-                              const uint8_t porep_proof_partitions) {
-    FFISectorClass sector_class;
-    sector_class.sector_size = sector_size;
-    sector_class.porep_proof_partitions = porep_proof_partitions;
-    return sector_class;
-  }*/
 
   FFICandidate cCandidate(const PoStCandidate &cpp_candidate) {
     FFICandidate c_candidate;
@@ -315,74 +264,6 @@ namespace fc::proofs {
     return res_ptr->is_valid;
   }
 
-  /*outcome::result<bool> Proofs::verifyPoSt(
-      uint64_t sector_size,
-      const SortedPublicSectorInfo &sector_info,
-      const Randomness &randomness,
-      uint64_t challenge_count,
-      gsl::span<const uint8_t> proof,
-      gsl::span<const Candidate> winners,
-      const Prover &prover_id) {
-    std::vector<uint64_t> sorted_sector_ids;
-    std::vector<uint8_t> flattening;
-    for (const auto &value : sector_info.values) {
-      sorted_sector_ids.push_back(value.sector_id);
-      std::copy(
-          value.comm_r.begin(), value.comm_r.end(), back_inserter(flattening));
-    }
-
-    std::vector<FFICandidate> c_winners = cCandidates(winners);
-
-    auto res_ptr = make_unique(verify_post(sector_size,
-                                           cPointerToArray(randomness),
-                                           challenge_count,
-                                           sorted_sector_ids.data(),
-                                           sorted_sector_ids.size(),
-                                           flattening.data(),
-                                           flattening.size(),
-                                           proof.data(),
-                                           proof.size(),
-                                           c_winners.data(),
-                                           c_winners.size(),
-                                           cPointerToArray(prover_id)),
-                               destroy_verify_post_response);
-
-    if (res_ptr->status_code != 0) {
-      logger_->error("verifyPoSt: " + std::string(res_ptr->error_msg));
-      return ProofsError::UNKNOWN;
-    }
-
-    return res_ptr->is_valid;
-  }
-
-  outcome::result<bool> Proofs::verifySeal(uint64_t sector_size,
-                                           const Comm &comm_r,
-                                           const Comm &comm_d,
-                                           const Prover &prover_id,
-                                           const Ticket &ticket,
-                                           const Seed &seed,
-                                           uint64_t sector_id,
-                                           gsl::span<const uint8_t> proof) {
-    auto res_ptr = make_unique(verify_seal(sector_size,
-                                           cPointerToArray(comm_r),
-                                           cPointerToArray(comm_d),
-                                           cPointerToArray(prover_id),
-                                           cPointerToArray(ticket),
-                                           cPointerToArray(seed),
-                                           sector_id,
-                                           proof.data(),
-                                           proof.size()),
-                               destroy_verify_seal_response);
-
-    if (res_ptr->status_code != 0) {
-      logger_->error("verifySeal: " + std::string(res_ptr->error_msg));
-
-      return ProofsError::UNKNOWN;
-    }
-
-    return res_ptr->is_valid;
-  }*/
-
   // ******************
   // GENERATED FUNCTIONS
   // ******************
@@ -441,105 +322,6 @@ namespace fc::proofs {
                  res_ptr->flattened_proofs_ptr
                      + res_ptr->flattened_proofs_len);  // NOLINT
   }
-
-  /*outcome::result<std::vector<Candidate>> Proofs::generateCandidates(
-      uint64_t sector_size,
-      const Prover &prover_id,
-      const Randomness &randomness,
-      uint64_t challenge_count,
-      const SortedPrivateSectorInfo &sorted_private_replica_info) {
-    std::vector<FFIPrivateReplicaInfo> c_sorted_private_sector_info =
-        cPrivateReplicasInfo(sorted_private_replica_info.values);
-
-    auto res_ptr =
-        make_unique(generate_candidates(sector_size,
-                                        cPointerToArray(randomness),
-                                        challenge_count,
-                                        c_sorted_private_sector_info.data(),
-                                        c_sorted_private_sector_info.size(),
-                                        cPointerToArray(prover_id)),
-                    destroy_generate_candidates_response);
-    if (res_ptr->status_code != 0) {
-      logger_->error("generateCandidates: " + std::string(res_ptr->error_msg));
-      return ProofsError::UNKNOWN;
-    }
-
-    return cppCandidates(gsl::span<const FFICandidate>(
-        res_ptr->candidates_ptr, res_ptr->candidates_len));
-  }
-
-  outcome::result<Proof> Proofs::generatePoSt(
-      uint64_t sectorSize,
-      const Blob<32> &prover_id,
-      const SortedPrivateSectorInfo &private_replica_info,
-      const Randomness &randomness,
-      gsl::span<const Candidate> winners) {
-    std::vector<FFICandidate> c_winners = cCandidates(winners);
-
-    std::vector<FFIPrivateReplicaInfo> c_sorted_private_sector_info =
-        cPrivateReplicasInfo(private_replica_info.values);
-
-    auto res_ptr =
-        make_unique(generate_post(sectorSize,
-                                  cPointerToArray(randomness),
-                                  c_sorted_private_sector_info.data(),
-                                  c_sorted_private_sector_info.size(),
-                                  c_winners.data(),
-                                  c_winners.size(),
-                                  cPointerToArray(prover_id)),
-                    destroy_generate_post_response);
-
-    if (res_ptr->status_code != 0) {
-      logger_->error("generatePoSt: " + std::string(res_ptr->error_msg));
-      return ProofsError::UNKNOWN;
-    }
-
-    return Proof(res_ptr->flattened_proofs_ptr,
-                 res_ptr->flattened_proofs_ptr
-                     + res_ptr->flattened_proofs_len);  // NOLINT
-  }
-
-  outcome::result<Comm> Proofs::generatePieceCommitmentFromFile(
-      const std::string &piece_file_path, uint64_t piece_size) {
-    int fd;
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg, hicpp-vararg)
-    if ((fd = open(piece_file_path.c_str(), O_RDWR)) == -1) {
-      return ProofsError::CANNOT_OPEN_FILE;
-    }
-
-    auto res_ptr = make_unique(generate_piece_commitment(fd, piece_size),
-                               destroy_generate_piece_commitment_response);
-
-    // NOLINTNEXTLINE(readability-implicit-bool-conversion)
-    if (close(fd))
-      logger_->warn("generatePieceCommitmentFromFile: error in closing file "
-                    + piece_file_path);
-
-    if (res_ptr->status_code != 0) {
-      logger_->error("generatePieceCommitmentFromFile: "
-                     + std::string(res_ptr->error_msg));
-      return ProofsError::UNKNOWN;
-    }
-
-    return cppCommitment(gsl::span(res_ptr->comm_p, kCommitmentBytesLen));
-  }
-
-  outcome::result<Comm> Proofs::generateDataCommitment(
-      uint64_t sector_size, gsl::span<const PublicPieceInfo> pieces) {
-    std::vector<FFIPublicPieceInfo> c_pieces = cPublicPiecesInfo(pieces);
-
-    auto res_ptr = make_unique(
-        generate_data_commitment(sector_size, c_pieces.data(), c_pieces.size()),
-        destroy_generate_data_commitment_response);
-
-    if (res_ptr->status_code != 0) {
-      logger_->error("generateDataCommitment: "
-                     + std::string(res_ptr->error_msg));
-      return ProofsError::UNKNOWN;
-    }
-
-    return cppCommitment(gsl::span(res_ptr->comm_d, kCommitmentBytesLen));
-  }*/
 
   outcome::result<WriteWithoutAlignmentResult> Proofs::writeWithoutAlignment(
       RegisteredProof proof_type,
@@ -695,102 +477,6 @@ namespace fc::proofs {
                               res_ptr->comm_d, kCommitmentBytesLen)));
   }
 
-  /*outcome::result<Proof> Proofs::sealCommit(
-      uint64_t sector_size,
-      uint8_t porep_proof_partitions,
-      const std::string &cache_dir_path,
-      uint64_t sector_id,
-      const Prover &prover_id,
-      const Ticket &ticket,
-      const Seed &seed,
-      gsl::span<const PublicPieceInfo> pieces,
-      const RawSealPreCommitOutput &rspco) {
-    std::vector<FFIPublicPieceInfo> c_pieces = cPublicPiecesInfo(pieces);
-
-    auto res_ptr = make_unique(
-        seal_commit(cSectorClass(sector_size, porep_proof_partitions),
-                    cache_dir_path.c_str(),
-                    sector_id,
-                    cPointerToArray(prover_id),
-                    cPointerToArray(ticket),
-                    cPointerToArray(seed),
-                    c_pieces.data(),
-                    c_pieces.size(),
-                    cRawSealPreCommitOutput(rspco)),
-        destroy_seal_commit_response);
-
-    if (res_ptr->status_code != 0) {
-      logger_->error("sealCommit: " + std::string(res_ptr->error_msg));
-      return ProofsError::UNKNOWN;
-    }
-
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-    return Proof(res_ptr->proof_ptr, res_ptr->proof_ptr + res_ptr->proof_len);
-  }
-
-  outcome::result<void> Proofs::unseal(uint64_t sector_size,
-                                       uint8_t porep_proof_partitions,
-                                       const std::string &cache_dir_path,
-                                       const std::string &sealed_sector_path,
-                                       const std::string &unseal_output_path,
-                                       uint64_t sector_id,
-                                       const Prover &prover_id,
-                                       const Ticket &ticket,
-                                       const Comm &comm_d) {
-    auto res_ptr =
-        make_unique(::unseal(cSectorClass(sector_size, porep_proof_partitions),
-                             cache_dir_path.c_str(),
-                             sealed_sector_path.c_str(),
-                             unseal_output_path.c_str(),
-                             sector_id,
-                             cPointerToArray(prover_id),
-                             cPointerToArray(ticket),
-                             cPointerToArray(comm_d)),
-                    destroy_unseal_response);
-
-    if (res_ptr->status_code != 0) {
-      logger_->error("unseal: " + std::string(res_ptr->error_msg));
-
-      return ProofsError::UNKNOWN;
-    }
-
-    return outcome::success();
-  }
-
-  outcome::result<void> Proofs::unsealRange(
-      uint64_t sector_size,
-      uint8_t porep_proof_partitions,
-      const std::string &cache_dir_path,
-      const std::string &sealed_sector_path,
-      const std::string &unseal_output_path,
-      uint64_t sector_id,
-      const Prover &prover_id,
-      const Ticket &ticket,
-      const Comm &comm_d,
-      uint64_t offset,
-      uint64_t length) {
-    auto res_ptr = make_unique(
-        unseal_range(cSectorClass(sector_size, porep_proof_partitions),
-                     cache_dir_path.c_str(),
-                     sealed_sector_path.c_str(),
-                     unseal_output_path.c_str(),
-                     sector_id,
-                     cPointerToArray(prover_id),
-                     cPointerToArray(ticket),
-                     cPointerToArray(comm_d),
-                     offset,
-                     length),
-        destroy_unseal_range_response);
-
-    if (res_ptr->status_code != 0) {
-      logger_->error("unsealRange: " + std::string(res_ptr->error_msg));
-
-      return ProofsError::UNKNOWN;
-    }
-
-    return outcome::success();
-  }
-*/
   SortedPrivateSectorInfo Proofs::newSortedPrivateSectorInfo(
       gsl::span<const PrivateSectorInfo> replica_info) {
     SortedPrivateSectorInfo sorted_replica_info;
@@ -817,42 +503,6 @@ namespace fc::proofs {
 
     return sorted_sector_info;
   }
-  /*
-    outcome::result<Ticket> Proofs::finalizeTicket(const Ticket &partial_ticket)
-    { auto res_ptr =
-    make_unique(finalize_ticket(cPointerToArray(partial_ticket)),
-                                 destroy_finalize_ticket_response);
-
-      if (res_ptr->status_code != 0) {
-        logger_->error("finalizeTicket: " + std::string(res_ptr->error_msg));
-        return ProofsError::UNKNOWN;
-      }
-
-      static const int kTicketSize = 32;
-
-      return cppCommitment(gsl::make_span(res_ptr->ticket, kTicketSize));
-    }
-
-    uint64_t Proofs::getMaxUserBytesPerStagedSector(uint64_t sector_size) {
-      return get_max_user_bytes_per_staged_sector(sector_size);
-    }
-
-    outcome::result<Devices> Proofs::getGPUDevices() {
-      auto res_ptr = make_unique(get_gpu_devices(),
-    destroy_gpu_device_response);
-
-      if (res_ptr->status_code != 0) {
-        logger_->error("getGPUDevices: " + std::string(res_ptr->error_msg));
-        return ProofsError::UNKNOWN;
-      }
-
-      if (res_ptr->devices_ptr == nullptr || res_ptr->devices_len == 0) {
-        return Devices();
-      }
-
-      return Devices(res_ptr->devices_ptr,
-                     res_ptr->devices_ptr + res_ptr->devices_len);  // NOLINT
-    }*/
 
   outcome::result<CID> Proofs::generatePieceCIDFromFile(
       RegisteredProof proof_type,
--- core/proofs/proofs.hpp
+++ core/proofs/proofs.hpp
@@ -6,9 +6,9 @@
 #ifndef CPP_FILECOIN_CORE_PROOFS_HPP
 #define CPP_FILECOIN_CORE_PROOFS_HPP
 
-#include <common/logger.hpp>
 #include <vector>
 #include "common/blob.hpp"
+#include "common/logger.hpp"
 #include "common/outcome.hpp"
 #include "crypto/randomness/randomness_types.hpp"
 #include "primitives/cid/cid.hpp"
@@ -40,14 +40,6 @@ namespace fc::proofs {
   using primitives::sector::SealRandomness;
   using primitives::sector::SectorInfo;
   using primitives::sector::Ticket;
-
-  // RawSealPreCommitOutput is used to acquire a seed from the chain for the
-  // second step of Interactive PoRep.
-  /*class RawSealPreCommitOutput {
-   public:
-    Comm comm_d;
-    Comm comm_r;
-  };*/
 
   struct PublicSectorInfo {
     RegisteredProof post_proof_type;
@@ -96,20 +88,20 @@ namespace fc::proofs {
     static fc::proofs::SortedPublicSectorInfo newSortedPublicSectorInfo(
         gsl::span<const PublicSectorInfo> sector_info);
 
-    static outcome::result<fc::proofs::WriteWithoutAlignmentResult>
-    writeWithoutAlignment(RegisteredProof proof_type,
-                          const std::string &piece_file_path,
-                          const UnpaddedPieceSize &piece_bytes,
-                          const std::string &staged_sector_file_path);
+    static outcome::result<WriteWithoutAlignmentResult> writeWithoutAlignment(
+        RegisteredProof proof_type,
+        const std::string &piece_file_path,
+        const UnpaddedPieceSize &piece_bytes,
+        const std::string &staged_sector_file_path);
 
     // existing_piece_sizes should be UnpaddedPieceSize, but for prevent copy
     // array it is uint64 span
-    static outcome::result<fc::proofs::WriteWithAlignmentResult>
-    writeWithAlignment(RegisteredProof proof_type,
-                       const std::string &piece_file_path,
-                       const UnpaddedPieceSize &piece_bytes,
-                       const std::string &staged_sector_file_path,
-                       gsl::span<const uint64_t> existing_piece_sizes);
+    static outcome::result<WriteWithAlignmentResult> writeWithAlignment(
+        RegisteredProof proof_type,
+        const std::string &piece_file_path,
+        const UnpaddedPieceSize &piece_bytes,
+        const std::string &staged_sector_file_path,
+        gsl::span<const uint64_t> existing_piece_sizes);
 
     /**
      * @brief  Seals the staged sector at staged_sector_path in place, saving
@@ -130,54 +122,6 @@ namespace fc::proofs {
         const std::string &cache_dir_path,
         const std::string &sealed_sector_path);
 
-    /*static outcome::result<Proof> sealCommit(
-        uint64_t sector_size,
-        uint8_t porep_proof_partitions,
-        const std::string &cache_dir_path,
-        uint64_t sector_id,
-        const Prover &prover_id,
-        const Ticket &ticket,
-        const Seed &seed,
-        gsl::span<const PublicPieceInfo> pieces,
-        const RawSealPreCommitOutput &rspco);*/
-
-    /**
-     * Unseals sector
-     */
-    /*static outcome::result<void> unseal(uint64_t sector_size,
-                                        uint8_t porep_proof_partitions,
-                                        const std::string &cache_dir_path,
-                                        const std::string &sealed_sector_path,
-                                        const std::string &unseal_output_path,
-                                        uint64_t sector_id,
-                                        const Prover &prover_id,
-                                        const Ticket &ticket,
-                                        const Comm &comm_d);*/
-
-    /**
-     * @brief Unseals the sector at @sealed_path and returns the bytes for a
-     * piece whose first (unpadded) byte begins at @offset and ends at @offset
-     * plus @num_bytes, inclusive
-     */
-    /*static outcome::result<void> unsealRange(
-        uint64_t sector_size,
-        uint8_t porep_proof_partitions,
-        const std::string &cache_dir_path,
-        const std::string &sealed_sector_path,
-        const std::string &unseal_output_path,
-        uint64_t sector_id,
-        const Prover &prover_id,
-        const Ticket &ticket,
-        const Comm &comm_d,
-        uint64_t offset,
-        uint64_t length);*/
-
-    /**
-     * @brief Computes a sectors's comm_d given its pieces
-     */
-    /*static outcome::result<Comm> generateDataCommitment(
-        uint64_t sector_size, gsl::span<const PublicPieceInfo> pieces);*/
-
     static outcome::result<CID> generatePieceCIDFromFile(
         RegisteredProof proof_type,
         const std::string &piece_file_path,
@@ -185,13 +129,6 @@ namespace fc::proofs {
 
     static outcome::result<CID> generateUnsealedCID(
         RegisteredProof proof_type, gsl::span<PieceInfo> pieces);
-
-    /**
-     * @brief Generates a piece commitment for the provided byte source. Returns
-     * an error if the byte source produced more than piece_size bytes
-     */
-    /*static outcome::result<Comm> generatePieceCommitmentFromFile(
-        const std::string &piece_file_path, uint64_t piece_size);*/
 
     /**
      * @brief Generates proof-of-spacetime candidates for ElectionPoSt
@@ -222,36 +159,6 @@ namespace fc::proofs {
         gsl::span<const uint8_t> proof,
         gsl::span<const PoStCandidate> winners,
         const Prover &prover_id);
-
-    /**
-     * @brief Verifies the output of some previous-run seal operation
-     */
-    /* static outcome::result<bool> verifySeal(uint64_t sector_size,
-                                             const Comm &comm_r,
-                                             const Comm &comm_d,
-                                             const Prover &prover_id,
-                                             const Ticket &ticket,
-                                             const Seed &seed,
-                                             uint64_t sector_id,
-                                             gsl::span<const uint8_t> proof);*/
-
-    /**
-     * @brief Generates a ticket from a @partial_ticket
-     */
-    // static outcome::result<Ticket> finalizeTicket(const Ticket
-    // &partial_ticket);
-
-    /**
-     * @brief Returns the number of user bytes that will fit into a staged
-     * sector
-     */
-    //  static uint64_t getMaxUserBytesPerStagedSector(uint64_t sector_size);
-
-    /**
-     * @brief Produces a vector of strings, each representing the name of a
-     * detected GPU device
-     */
-    //   static outcome::result<Devices> getGPUDevices();
 
    private:
     static fc::common::Logger logger_;
