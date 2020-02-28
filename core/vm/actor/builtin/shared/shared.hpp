c19756b1509d8f60755a6ac6d2fc1693e151283b
turuslan <turuslan.devbox@gmail.com>
2020-02-28 16:44:07 +0300

    explicit return type
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- core/vm/actor/builtin/shared/shared.hpp
+++ core/vm/actor/builtin/shared/shared.hpp
@@ -24,7 +24,8 @@ namespace fc::vm::actor::builtin {
    * @param miner
    * @return
    */
-  auto requestMinerControlAddress(Runtime &runtime, const Address &miner) {
+  outcome::result<miner::ControlAddresses::Result> requestMinerControlAddress(
+      Runtime &runtime, const Address &miner) {
     return runtime.sendM<miner::ControlAddresses>(miner, {}, 0);
   }
 }  // namespace fc::vm::actor::builtin
