0e88932026f6a390c3f8e089fdbbf026f48c47b8
turuslan <turuslan.devbox@gmail.com>
2020-03-13 11:03:21 +0300

    json errors
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

---
+++ core/api/rpc/json_errors.hpp
@@ -0,0 +1,22 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CPP_FILECOIN_CORE_API_RPC_JSON_ERRORS_HPP
+#define CPP_FILECOIN_CORE_API_RPC_JSON_ERRORS_HPP
+
+#include "common/outcome.hpp"
+
+namespace fc::api {
+  enum class JsonError {
+    WRONG_LENGTH = 1,
+    WRONG_ENUM,
+    WRONG_TYPE,
+    OUT_OF_RANGE,
+  };
+}  // namespace fc::api
+
+OUTCOME_HPP_DECLARE_ERROR(fc::api, JsonError);
+
+#endif  // CPP_FILECOIN_CORE_API_RPC_JSON_ERRORS_HPP
