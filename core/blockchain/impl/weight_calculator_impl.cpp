85a097e022175e0717279781dc089c287ecdc03f
turuslan <turuslan.devbox@gmail.com>
2020-03-21 17:20:10 +0300

    weight calculator impl
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- core/blockchain/impl/weight_calculator_impl.cpp
+++ core/blockchain/impl/weight_calculator_impl.cpp
@@ -5,13 +5,46 @@
 
 #include "blockchain/impl/weight_calculator_impl.hpp"
 
+#include "vm/actor/builtin/storage_power/storage_power_actor_state.hpp"
+#include "vm/state/impl/state_tree_impl.hpp"
+
+/* LCOV_EXCL_START */
+OUTCOME_CPP_DEFINE_CATEGORY(fc::blockchain::weight, WeightCalculatorError, e) {
+  using E = fc::blockchain::weight::WeightCalculatorError;
+  switch (e) {
+    case E::NO_NETWORK_POWER:
+      return "No network power";
+  }
+}
+/* LCOV_EXCL_STOP */
+
 namespace fc::blockchain::weight {
   using primitives::BigInt;
+  using vm::actor::kStoragePowerAddress;
+  using vm::actor::builtin::storage_power::StoragePowerActorState;
+  using vm::state::StateTreeImpl;
+
+  constexpr uint64_t kWRatioNum{1};
+  constexpr uint64_t kWRatioDen{2};
+  constexpr uint64_t kBlocksPerEpoch{5};
+
+  WeightCalculatorImpl::WeightCalculatorImpl(std::shared_ptr<Ipld> ipld)
+      : ipld_{std::move(ipld)} {}
 
   outcome::result<BigInt> WeightCalculatorImpl::calculateWeight(
       const Tipset &tipset) {
-    // TODO(yuraz): FIL-155 implement weight calculator, this one is stub
-    return 1;
+    OUTCOME_TRY(actor,
+                StateTreeImpl{ipld_, tipset.getParentStateRoot()}.get(
+                    kStoragePowerAddress));
+    OUTCOME_TRY(state, ipld_->getCbor<StoragePowerActorState>(actor.head));
+    auto network_power = state.total_network_power;
+    if (network_power <= 0) {
+      return outcome::failure(WeightCalculatorError::NO_NETWORK_POWER);
+    }
+    BigInt log{boost::multiprecision::msb(network_power) << 8};
+    return tipset.getParentWeight() + log
+           + (log * tipset.blks.size() * kWRatioNum)
+                 / (kBlocksPerEpoch * kWRatioDen);
   }
 
 }  // namespace fc::blockchain::weight
