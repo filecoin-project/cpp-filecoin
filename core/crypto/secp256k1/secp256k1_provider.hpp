d47935296de7d402708aff57efc28a46f778bb1b
Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>
2020-04-14 17:14:24 +0300

    add submodule secp256k1 for pubkey recovery
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>

--- core/crypto/secp256k1/secp256k1_provider.hpp
+++ core/crypto/secp256k1/secp256k1_provider.hpp
@@ -6,18 +6,36 @@
 #ifndef CPP_FILECOIN_TEST_CORE_CRYPTO_SECP256K1_PROVIDER_SECP256K1_PROVIDER_HPP
 #define CPP_FILECOIN_TEST_CORE_CRYPTO_SECP256K1_PROVIDER_SECP256K1_PROVIDER_HPP
 
+#include <gsl/span>
 #include <libp2p/crypto/error.hpp>
 #include <libp2p/crypto/secp256k1_provider/secp256k1_provider_impl.hpp>
+#include "common/outcome.hpp"
 
 namespace fc::crypto::secp256k1 {
+
   using libp2p::crypto::secp256k1::KeyPair;
   using libp2p::crypto::secp256k1::kPrivateKeyLength;
   using libp2p::crypto::secp256k1::kPublicKeyLength;
   using libp2p::crypto::secp256k1::PrivateKey;
   using libp2p::crypto::secp256k1::PublicKey;
-  using libp2p::crypto::secp256k1::Secp256k1Provider;
-  using libp2p::crypto::secp256k1::Secp256k1ProviderImpl;
   using libp2p::crypto::secp256k1::Signature;
+
+  /**
+   * Interface is expanded with pubkey recovery from signature.
+   */
+  class Secp256k1Provider
+      : public libp2p::crypto::secp256k1::Secp256k1Provider {
+   public:
+    /**
+     * RecoverPubkey returns the the public key of the signer.
+     * @param message - signed data
+     * @param signature - target for verifying
+     * @return Derived public key or error code
+     */
+    virtual outcome::result<PublicKey> recoverPublicKey(
+        gsl::span<const uint8_t> message, const Signature &signature) const = 0;
+  };
+
 }  // namespace fc::crypto::secp256k1
 
 #endif  // CPP_FILECOIN_TEST_CORE_CRYPTO_SECP256K1_PROVIDER_SECP256K1_PROVIDER_HPP
---
+++ deps/libsecp256k1.cmake
@@ -0,0 +1,10 @@
+#
+# Copyright Soramitsu Co., Ltd. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_library(libsecp256k1
+    libsecp256k1/src/gen_context.c
+    libsecp256k1/src/secp256k1.c
+    )
+disable_clang_tidy(libsecp256k1)
