5015a8313935ffbae79afd0890ecd72a6d23a8ad
turuslan <turuslan.devbox@gmail.com>
2020-04-30 14:22:48 +0300

    simple pb dag decoder and recursive all
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- core/storage/ipld/walker.cpp
+++ core/storage/ipld/walker.cpp
@@ -5,7 +5,60 @@
 
 #include "storage/ipld/walker.hpp"
 
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl_lite.h>
+
+#include "common/todo_error.hpp"
+
 namespace fc::storage::ipld::walker {
+  using google::protobuf::io::CodedInputStream;
+  using Input = gsl::span<const uint8_t>;
+
+  struct PbDecoder {
+    PbDecoder(Input input) : input{input}, cs(input.data(), input.size()) {}
+
+    Input _str(uint64_t id) {
+      if (cs.ExpectTag((id << 3) | 2)) {
+        int size;
+        if (cs.ReadVarintSizeAsInt(&size)) {
+          auto offset = cs.CurrentPosition();
+          if (cs.Skip(size)) {
+            return input.subspan(offset, size);
+          }
+        }
+      }
+      return {};
+    }
+
+    bool empty() {
+      return cs.CurrentPosition() >= input.size();
+    }
+
+    Input input;
+    CodedInputStream cs;
+  };
+
+  struct PbNodeDecoder {
+    static outcome::result<std::vector<CID>> links(
+        gsl::span<const uint8_t> input) {
+      std::vector<CID> cids;
+      PbDecoder s{input};
+      while (true) {
+        PbDecoder link{s._str(2)};
+        if (link.empty()) {
+          break;
+        }
+        auto cid_bytes = link._str(1);
+        if (cid_bytes.empty()) {
+          return TodoError::ERROR;
+        }
+        OUTCOME_TRY(cid, CID::fromBytes(cid_bytes));
+        cids.push_back(std::move(cid));
+      }
+      return std::move(cids);
+    }
+  };
+
   outcome::result<void> Walker::select(const CID &root,
                                        const Selector &selector) {
     // TODO(turuslan): implement selectors
@@ -16,13 +69,17 @@ namespace fc::storage::ipld::walker {
     if (cids.insert(cid).second) {
       OUTCOME_TRY(bytes, store.get(cid));
       // TODO(turuslan): what about other types?
-      // TODO(turuslan): DAG_PB, if required
       if (cid.content_type == libp2p::multi::MulticodecType::DAG_CBOR) {
         try {
           CborDecodeStream s{bytes};
           recursiveAll(s);
         } catch (std::system_error &e) {
           return outcome::failure(e.code());
+        }
+      } else if (cid.content_type == libp2p::multi::MulticodecType::DAG_PB) {
+        OUTCOME_TRY(cids, PbNodeDecoder::links(bytes));
+        for (auto &cid : cids) {
+          OUTCOME_TRY(recursiveAll(cid));
         }
       }
     }
