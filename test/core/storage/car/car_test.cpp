e3091f31e54f4581dacba534a8d0ce7472636925
Alexey <Alexey.N.Chernyshov@gmail.com>
2020-04-30 18:03:40 +0300

    Feature/selective car (#180)
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>
    
    Co-authored-by: Ruslan Tushov <turuslan@users.noreply.github.com>

--- test/core/storage/car/car_test.cpp
+++ test/core/storage/car/car_test.cpp
@@ -10,6 +10,7 @@
 #include <gtest/gtest.h>
 
 #include "storage/ipfs/impl/in_memory_datastore.hpp"
+#include "storage/unixfs/unixfs.hpp"
 #include "testutil/literals.hpp"
 #include "testutil/outcome.hpp"
 #include "testutil/read_file.hpp"
@@ -18,6 +19,7 @@ using fc::CID;
 using fc::storage::car::CarError;
 using fc::storage::car::loadCar;
 using fc::storage::car::makeCar;
+using fc::storage::car::makeSelectiveCar;
 using fc::storage::ipfs::InMemoryDatastore;
 
 /**
@@ -76,4 +78,27 @@ TEST(CarTest, Writer) {
   EXPECT_OUTCOME_EQ(ipld2.get(cid2), raw2);
   EXPECT_OUTCOME_TRUE(raw3, ipld1.get(cid3));
   EXPECT_OUTCOME_EQ(ipld2.get(cid3), raw3);
+}
+
+/**
+ * Interop test with go-fil-markets/storagemarket/integration_test.go
+ * @given PAYLOAD_FILE with some data, cid_root of dag and selective_car bytes
+ * from go implementation CAR_FROM_PAYLOAD_FILE
+ * @when make selective_car file from PAYLOAD_FILE data
+ * @then selective_car bytes are equal to CAR_FROM_PAYLOAD_FILE
+ */
+TEST(SelectiveCar, MakeSelectiveCar) {
+  std::string root_cid_expected{
+      "QmXFz92Uc9gCyAVGKkCzD84HEiR9fmrFzPSrvUypaN2Yzx"};
+
+  InMemoryDatastore ipld;
+  auto input = readFile(PAYLOAD_FILE);
+  EXPECT_OUTCOME_TRUE(root_cid, fc::storage::unixfs::wrapFile(ipld, input));
+  EXPECT_OUTCOME_EQ(root_cid.toString(), root_cid_expected);
+  EXPECT_OUTCOME_TRUE(selective_car, makeSelectiveCar(ipld, {{root_cid, {}}}));
+
+  auto expected_car = readFile(CAR_FROM_PAYLOAD_FILE);
+  EXPECT_EQ(selective_car, expected_car) << std::endl
+                                         << selective_car << std::endl
+                                         << expected_car << std::endl;
 }
