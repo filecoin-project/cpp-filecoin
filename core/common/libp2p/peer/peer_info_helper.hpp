3bfa3589dff6dc3cfe77f891b7e60689b3050a27
Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>
2020-05-09 21:01:45 +0300

    provider pipeline
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>

---
+++ core/common/libp2p/peer/peer_info_helper.hpp
@@ -0,0 +1,27 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CPP_FILECOIN_CORE_COMMON_LIBP2P_PEER_PEER_INFO_HELPER_HPP
+#define CPP_FILECOIN_CORE_COMMON_LIBP2P_PEER_PEER_INFO_HELPER_HPP
+
+#include <libp2p/peer/peer_info.hpp>
+#include <sstream>
+
+using libp2p::peer::PeerInfo;
+
+/**
+ * Represent peer info as human readable string
+ * @param peer_info to prettify
+ * @return string
+ */
+std::string peerInfoToPrettyString(const PeerInfo &peer_info) {
+  std::stringstream ss;
+  for (const auto &address : peer_info.addresses) {
+    ss << std::string(address.getStringAddress()) << " ";
+  }
+  return ss.str();
+}
+
+#endif  // CPP_FILECOIN_CORE_COMMON_LIBP2P_PEER_PEER_INFO_HELPER_HPP
