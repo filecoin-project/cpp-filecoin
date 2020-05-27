a775672e384ab68e58c92099cbea14e6871c0422
artyom-yurin <artem_yrin@mail.ru>
2020-05-27 10:12:34 +0300

    Added Soramitsu license header into uri parser files
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

--- core/primitives/uri_parser/percent_encoding.cpp
+++ core/primitives/uri_parser/percent_encoding.cpp
@@ -1,3 +1,8 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 // Copyright © 2017-2019 Dmitriy Khaustov
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
