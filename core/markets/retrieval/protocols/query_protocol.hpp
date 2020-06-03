2c07de4975ef878d564674d4e78e200de8f0e758
Sergey Kaprovich <serginetty@gmail.com>
2020-06-03 12:51:13 +0300

    Retrieval market: query implementation

---
+++ core/markets/retrieval/protocols/query_protocol.hpp
@@ -0,0 +1,118 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CPP_FILECOIN_CORE_MARKETS_RETRIEVAL_QUERY_PROTOCOL_HPP
+#define CPP_FILECOIN_CORE_MARKETS_RETRIEVAL_QUERY_PROTOCOL_HPP
+
+#include <string>
+
+#include <libp2p/peer/protocol.hpp>
+#include "codec/cbor/streams_annotation.hpp"
+#include "primitives/address/address.hpp"
+#include "primitives/address/address_codec.hpp"
+#include "primitives/cid/cid.hpp"
+#include "primitives/types.hpp"
+
+namespace fc::markets::retrieval {
+  using primitives::TokenAmount;
+  using primitives::address::Address;
+
+  /**
+   * @brief Query Protocol ID V0
+   */
+  const libp2p::peer::Protocol kQueryProtocolId = "/fil/retrieval/qry/0.0.1";
+
+  /**
+   * @struct Request from client to provider
+   *         to retrieve specified data by CID
+   */
+  struct QueryParams {
+    /* Identifier of the parent's Piece */
+    CID piece_cid;
+  };
+
+  struct QueryRequest {
+    /* Identifier of the requested item */
+    CID payload_cid;
+
+    /* Additional params */
+    QueryParams params;
+  };
+
+  CBOR_TUPLE(QueryParams, piece_cid)
+
+  CBOR_TUPLE(QueryRequest, payload_cid, params)
+
+  /**
+   * @enum Status of the query response
+   */
+  enum class QueryResponseStatus : uint64_t {
+    /* Provider has a piece and is prepared to return it */
+    QueryResponseAvailable = 1,
+
+    /* Provider either does not have a piece or cannot serve request */
+    QueryResponseUnavailable,
+
+    /* Something went wrong generating a query response */
+    QueryResponseError
+  };
+
+  /**
+   * @enum Status of the queried item
+   */
+  enum class QueryItemStatus : uint64_t {
+    /* Requested part of the piece is available to be served */
+    QueryItemAvailable = 1,
+
+    /* Requested part of the piece is unavailable or cannot be served */
+    QueryItemUnavailable,
+
+    /* Cannot determine if the given item is part of the requested piece */
+    QueryItemUnknown
+  };
+
+  /**
+   * @struct Response from provider with initial retrieval params
+   */
+  struct QueryResponse {
+    /* Current response status */
+    QueryResponseStatus response_status;
+
+    /* Current item status */
+    QueryItemStatus item_status;
+
+    /* Size of the requested piece in bytes */
+    size_t item_size;
+
+    /* Address to send tokens, may be different than miner address */
+    Address payment_address;
+
+    /* Min token amount per byte */
+    TokenAmount min_price_per_byte;
+
+    /* Max number of bytes a provider will send before requesting next payment
+     */
+    uint64_t payment_interval;
+
+    /* Max rate at which previous value increases */
+    uint64_t interval_increase;
+
+    /* Optional text message */
+    std::string message;
+  };
+
+  CBOR_TUPLE(QueryResponse,
+             response_status,
+             item_status,
+             item_size,
+             payment_address,
+             min_price_per_byte,
+             payment_interval,
+             interval_increase,
+             message);
+
+}  // namespace fc::markets::retrieval
+
+#endif
---
+++ test/core/markets/retrieval/fixture.hpp
@@ -0,0 +1,106 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "markets/retrieval/client/retrieval_client_impl.hpp"
+#include "markets/retrieval/provider/retrieval_provider_impl.hpp"
+
+#include <gtest/gtest.h>
+#include <libp2p/injector/host_injector.hpp>
+#include <libp2p/security/plaintext.hpp>
+#include "api/api.hpp"
+#include "core/markets/retrieval/config.hpp"
+#include "core/markets/retrieval/data.hpp"
+#include "storage/in_memory/in_memory_storage.hpp"
+#include "storage/piece/impl/piece_storage_impl.hpp"
+
+namespace fc::markets::retrieval::test {
+  struct RetrievalMarketFixture : public ::testing::Test {
+    /* Types */
+    using ClientShPtr = std::shared_ptr<client::RetrievalClientImpl>;
+    using ProviderShPtr = std::shared_ptr<provider::RetrievalProviderImpl>;
+    using ContextShPtr = std::shared_ptr<boost::asio::io_context>;
+    using HostShPtr = std::shared_ptr<libp2p::Host>;
+    using PieceStorageShPtr =
+        std::shared_ptr<::fc::storage::piece::PieceStorage>;
+    using StorageShPtr = std::shared_ptr<::fc::storage::InMemoryStorage>;
+    using Multiaddress = libp2p::multi::Multiaddress;
+    using ApiShPtr = std::shared_ptr<api::Api>;
+
+    /* Retrieval market client */
+    ClientShPtr client;
+
+    /* Retrieval market provider */
+    ProviderShPtr provider;
+
+    /* Libp2p network host */
+    HostShPtr host;
+
+    /* I/O context */
+    ContextShPtr context;
+
+    /* Piece storage */
+    PieceStorageShPtr piece_storage;
+
+    /* Common storage backend */
+    StorageShPtr storage_backend;
+
+    /* Common application interface */
+    ApiShPtr api;
+
+    /**
+     * @brief Constructor
+     */
+    RetrievalMarketFixture() {
+      auto injector = libp2p::injector::makeHostInjector(
+          libp2p::injector::useKeyPair(config::provider::keypair),
+          libp2p::injector::useSecurityAdaptors<libp2p::security::Plaintext>());
+      host = injector.create<std::shared_ptr<libp2p::Host>>();
+      context = injector.create<std::shared_ptr<boost::asio::io_context>>();
+      storage_backend = std::make_shared<::fc::storage::InMemoryStorage>();
+      api = std::make_shared<api::Api>();
+      this->piece_storage =
+          std::make_shared<::fc::storage::piece::PieceStorageImpl>(
+              storage_backend);
+      provider = std::make_shared<provider::RetrievalProviderImpl>(
+          host, piece_storage, api);
+      client = std::make_shared<client::RetrievalClientImpl>(host);
+      provider->start();
+    }
+
+    /**
+     * @brief On before all test cases
+     */
+    void SetUp() override {
+      auto listen_address =
+          Multiaddress::create(config::provider::multiaddress);
+      BOOST_ASSERT_MSG(listen_address.has_value(),
+                       "Failed to create server multiaddress");
+      auto listen_status = host->listen(listen_address.value());
+      BOOST_ASSERT_MSG(listen_status.has_value(),
+                       "Failed to listen on provider multiaddress");
+      host->start();
+      std::thread([this]() { context->run(); }).detach();
+      BOOST_ASSERT_MSG(addPieceSample(data::green_piece).has_value(),
+                       "Failed to add sample green piece");
+      api->WalletDefaultAddress = {[=]() { return config::provider::wallet; }};
+    }
+
+    /**
+     * @brief On all test finished
+     */
+    void TearDown() override {
+      context->stop();
+    }
+
+    outcome::result<void> addPieceSample(const SamplePiece &piece) {
+      OUTCOME_TRY(piece_storage->addPieceInfo(piece.cid, piece.info));
+      for (const auto &payload : piece.payloads) {
+        OUTCOME_TRY(piece_storage->addPayloadLocations(
+            piece.cid, {{payload.cid, payload.location}}));
+      }
+      return outcome::success();
+    }
+  };
+}  // namespace fc::markets::retrieval::test
