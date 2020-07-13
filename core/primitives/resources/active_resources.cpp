3bfcb8cb70c87d901b9cc23699f7fa49b2bbbd39
artyom-yurin <artem_yrin@mail.ru>
2020-07-13 16:36:39 +0300

    Implemented active resources logic
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

--- core/primitives/resources/active_resources.cpp
+++ core/primitives/resources/active_resources.cpp
@@ -6,41 +6,113 @@
 #include "primitives/resources/active_resources.hpp"
 
 namespace fc::primitives {
-    bool canHandleRequest(const Resources &need_resources,
-                          const WorkerResources &resources,
-                          const ActiveResources &active) {
-        // TODO: dedupe need_resources.base_min_memory per task type
-        // (don't add if that task is already running)
-        uint64_t min_need_memory =
-                resources.reserved_memory + active.memory_used_min
-                + need_resources.min_memory + need_resources.base_min_memory;
-        if (min_need_memory > resources.physical_memory) {
-            return false;
-        }
-
-        uint64_t max_need_memory =
-                resources.reserved_memory + active.memory_used_min
-                + need_resources.max_memory + need_resources.base_min_memory;
-        if (max_need_memory > (resources.swap_memory + resources.physical_memory)) {
-            return false;
-        }
-
-        if (need_resources.threads) {
-            if ((active.cpu_use + *need_resources.threads) > resources.cpus) {
-                return false;
-            }
-        } else {
-            if (active.cpu_use > 0) {
-                return false;
-            }
-        }
-
-        if (!resources.gpus.empty() && need_resources.can_gpu) {
-            if (active.gpu_used) {
-                return false;
-            }
-        }
-
-        return true;
+  bool canHandleRequest(const Resources &need_resources,
+                        const WorkerResources &resources,
+                        const ActiveResources &active) {
+    // TODO: dedupe need_resources.base_min_memory per task type
+    // (don't add if that task is already running)
+    uint64_t min_need_memory =
+        resources.reserved_memory + active.memory_used_min
+        + need_resources.min_memory + need_resources.base_min_memory;
+    if (min_need_memory > resources.physical_memory) {
+      return false;
     }
-}
+
+    uint64_t max_need_memory =
+        resources.reserved_memory + active.memory_used_min
+        + need_resources.max_memory + need_resources.base_min_memory;
+    if (max_need_memory > (resources.swap_memory + resources.physical_memory)) {
+      return false;
+    }
+
+    if (need_resources.threads) {
+      if ((active.cpu_use + *need_resources.threads) > resources.cpus) {
+        return false;
+      }
+    } else {
+      if (active.cpu_use > 0) {
+        return false;
+      }
+    }
+
+    if (!resources.gpus.empty() && need_resources.can_gpu) {
+      if (active.gpu_used) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  void ActiveResources::add(const WorkerResources &worker_resources,
+                            const Resources &resources) {
+    gpu_used = resources.can_gpu;
+    if (resources.threads) {
+      cpu_use += *resources.threads;
+    } else {
+      cpu_use += worker_resources.cpus;
+    }
+
+    memory_used_min += resources.min_memory;
+    memory_used_max += resources.max_memory;
+  }
+
+  void ActiveResources::free(const WorkerResources &worker_resources,
+                             const Resources &resources) {
+    if (resources.can_gpu) {
+      gpu_used = false;
+    }
+
+    if (resources.threads) {
+      cpu_use -= *resources.threads;
+    } else {
+      cpu_use -= worker_resources.cpus;
+    }
+
+    memory_used_min -= resources.min_memory;
+    memory_used_max -= resources.max_memory;
+  }
+
+  outcome::result<void> ActiveResources::withResources(
+      const WorkerResources &worker_resources,
+      const Resources &resources,
+      const std::function<outcome::result<void>()> &callback) {
+    while (!canHandleRequest(resources, worker_resources, *this)) {
+      std::unique_lock<std::mutex> lock(mutex_);
+      cv_.wait(lock, [&]() { return unlock_; });
+    }
+    unlock_ = false;
+
+    add(worker_resources, resources);
+
+    auto res = callback();
+
+    free(worker_resources, resources);
+
+    unlock_ = true;
+    cv_.notify_all();
+
+    return res;
+  }
+
+  double ActiveResources::utilization(const WorkerResources &worker_resources) {
+    double max = static_cast<double>(cpu_use) / worker_resources.cpus;
+
+    double memory_min =
+        static_cast<double>(memory_used_min + worker_resources.reserved_memory)
+        / worker_resources.physical_memory;
+    if (memory_min > max) {
+      max = memory_min;
+    }
+
+    double memory_max =
+        static_cast<double>(memory_used_max + worker_resources.reserved_memory)
+        / static_cast<double>(worker_resources.physical_memory
+                              + worker_resources.swap_memory);
+    if (memory_max > max) {
+      max = memory_max;
+    }
+
+    return max;
+  }
+}  // namespace fc::primitives
--- core/primitives/resources/active_resources.hpp
+++ core/primitives/resources/active_resources.hpp
@@ -7,6 +7,7 @@
 #define CPP_FILECOIN_CORE_PRIMITIVES_RESOURCES_ACTIVE_RESOURCES_HPP
 
 #include "primitives/resources/resources.hpp"
+#include "primitives/types.hpp"
 
 namespace fc::primitives {
 
@@ -15,6 +16,27 @@ namespace fc::primitives {
     uint64_t memory_used_max;
     bool gpu_used;
     uint64_t cpu_use;
+
+    void add(const WorkerResources &worker_resources,
+             const Resources &resources);
+
+    void free(const WorkerResources &worker_resources,
+              const Resources &resources);
+
+    /**
+     * @brief run @callback with @resources
+     */
+    outcome::result<void> withResources(
+        const WorkerResources &worker_resources,
+        const Resources &resources,
+        const std::function<outcome::result<void>()> &callback);
+
+    double utilization(const WorkerResources &worker_resources);
+
+   private:
+    std::mutex mutex_;
+    bool unlock_;
+    std::condition_variable cv_;
   };
 
   bool canHandleRequest(const Resources &need_resources,
