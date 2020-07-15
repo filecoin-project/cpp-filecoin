928b23e6b76b17a735f0f65bc5bc2456c97de155
artyom-yurin <artem_yrin@mail.ru>
2020-07-15 13:16:51 +0300

    Fixed error format in CBOR
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

--- core/codec/cbor/cbor_errors.cpp
+++ core/codec/cbor/cbor_errors.cpp
@@ -8,9 +8,9 @@
 OUTCOME_CPP_DEFINE_CATEGORY(fc::codec::cbor, CborEncodeError, e) {
   using fc::codec::cbor::CborEncodeError;
   switch (e) {
-    case CborEncodeError::INVALID_CID:
+    case CborEncodeError::kInvalidCID:
       return "Invalid CID";
-    case CborEncodeError::EXPECTED_MAP_VALUE_SINGLE:
+    case CborEncodeError::kExpectedMapValueSingle:
       return "Expected map value single";
     default:
       return "Unknown error";
@@ -20,17 +20,17 @@ OUTCOME_CPP_DEFINE_CATEGORY(fc::codec::cbor, CborEncodeError, e) {
 OUTCOME_CPP_DEFINE_CATEGORY(fc::codec::cbor, CborDecodeError, e) {
   using fc::codec::cbor::CborDecodeError;
   switch (e) {
-    case CborDecodeError::INVALID_CBOR:
+    case CborDecodeError::kInvalidCbor:
       return "Invalid CBOR";
-    case CborDecodeError::WRONG_TYPE:
+    case CborDecodeError::kWrongType:
       return "Wrong type";
-    case CborDecodeError::INT_OVERFLOW:
+    case CborDecodeError::kIntOverflow:
       return "Int overflow";
-    case CborDecodeError::INVALID_CBOR_CID:
+    case CborDecodeError::kInvalidCborCID:
       return "Invalid CBOR CID";
-    case CborDecodeError::INVALID_CID:
+    case CborDecodeError::kInvalidCID:
       return "Invalid CID";
-    case CborDecodeError::WRONG_SIZE:
+    case CborDecodeError::kWrongSize:
       return "Wrong size";
     default:
       return "Unknown error";
