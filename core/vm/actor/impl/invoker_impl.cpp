1b5e6966b9ad1394c6e26d83ee67097ce7ba1274
Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>
2020-12-07 10:51:41 +0300

    add cron actor v2 to cpp invoker
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>

--- core/vm/actor/impl/invoker_impl.cpp
+++ core/vm/actor/impl/invoker_impl.cpp
@@ -16,12 +16,16 @@
 #include "vm/actor/builtin/v0/reward/reward_actor.hpp"
 #include "vm/actor/builtin/v0/storage_power/storage_power_actor_export.hpp"
 #include "vm/actor/builtin/v0/system/system_actor.hpp"
+#include "vm/actor/builtin/v2/account/account_actor.hpp"
+#include "vm/actor/builtin/v2/codes.hpp"
+#include "vm/actor/builtin/v2/cron/cron_actor.hpp"
 #include "vm/actor/cgo/actors.hpp"
 
 namespace fc::vm::actor {
   using runtime::InvocationOutput;
 
   InvokerImpl::InvokerImpl() {
+    // v0
     builtin_[builtin::v0::kInitCodeCid] = builtin::v0::init::exports;
     builtin_[builtin::v0::kRewardActorCodeID] = builtin::v0::reward::exports;
     builtin_[builtin::v0::kCronCodeCid] = builtin::v0::cron::exports;
@@ -34,6 +38,10 @@ namespace fc::vm::actor {
         builtin::v0::payment_channel::exports;
     builtin_[builtin::v0::kAccountCodeCid] = builtin::v0::account::exports;
     builtin_[builtin::v0::kSystemActorCodeID] = builtin::system::exports;
+
+    // v2
+    builtin_[builtin::v2::kAccountCodeCid] = builtin::v2::account::exports;
+    builtin_[builtin::v2::kCronCodeCid] = builtin::v2::cron::exports;
   }
 
   void InvokerImpl::config(
@@ -46,17 +54,31 @@ namespace fc::vm::actor {
   outcome::result<InvocationOutput> InvokerImpl::invoke(
       const Actor &actor, const std::shared_ptr<Runtime> &runtime) {
     // TODO (a.chernyshov) remove after all cpp actors are implemented
-    if ((actor.code != builtin::v0::kAccountCodeCid)            // < tested OK
-        && (actor.code != builtin::v0::kCronCodeCid)            // < tested OK
-        && (actor.code != builtin::v0::kInitCodeCid)            // < tested OK
-        && (actor.code != builtin::v0::kStorageMarketCodeCid)   // < tested OK
-//        && (actor.code != builtin::v0::kStorageMinerCodeCid)    // TODO
-//        && (actor.code != builtin::v0::kMultisigCodeCid)        // TODO
-//        && (actor.code != builtin::v0::kPaymentChannelCodeCid)  // TODO
-//        && (actor.code != builtin::v0::kStoragePowerCodeCid)    // < WiP
-//        && (actor.code != builtin::v0::kRewardActorCodeID)      // TODO
-        && (actor.code != builtin::v0::kSystemActorCodeID)      // < tested OK
-//        && (actor.code != builtin::v0::kVerifiedRegistryCode)   // TODO
+    if (
+        // v0
+        (actor.code != builtin::v0::kAccountCodeCid)           // < tested OK
+        && (actor.code != builtin::v0::kCronCodeCid)           // < tested OK
+        && (actor.code != builtin::v0::kInitCodeCid)           // < tested OK
+        && (actor.code != builtin::v0::kStorageMarketCodeCid)  // < tested OK
+        // && (actor.code != builtin::v0::kStorageMinerCodeCid)    // TODO
+        // && (actor.code != builtin::v0::kMultisigCodeCid) // TODO
+        // && (actor.code != builtin::v0::kPaymentChannelCodeCid)  // TODO
+        // && (actor.code != builtin::v0::kStoragePowerCodeCid)    // < WiP
+        // && (actor.code != builtin::v0::kRewardActorCodeID)      // TODO
+        && (actor.code != builtin::v0::kSystemActorCodeID)     // < tested OK
+        && (actor.code != builtin::v0::kVerifiedRegistryCode)  // TODO
+        // v2
+        && (actor.code != builtin::v2::kAccountCodeCid)  // < tested OK
+        && (actor.code != builtin::v2::kCronCodeCid)     // < tested OK
+        // && (actor.code != builtin::v2::kInitCodeCid)           // TODO
+        // && (actor.code != builtin::v2::kStorageMarketCodeCid)  // TODO
+        // && (actor.code != builtin::v2::kStorageMinerCodeCid)    // TODO
+        // && (actor.code != builtin::v2::kMultisigCodeCid)        // TODO
+        // && (actor.code != builtin::v2::kPaymentChannelCodeCid)  // TODO
+        // && (actor.code != builtin::v2::kStoragePowerCodeCid)   // TODO
+        // && (actor.code != builtin::v2::kRewardActorCodeID)     // TODO
+        // && (actor.code != builtin::v2::kSystemActorCodeID)     // TODO
+        // && (actor.code != builtin::v2::kVerifiedRegistryCode)  // TODO
     ) {
       return ::fc::vm::actor::cgo::invoke(actor.code, runtime);
     }
