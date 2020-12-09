0d13c5310ba1e44e5315307038b783d32ab0900b
Alexey <Alexey.N.Chernyshov@gmail.com>
2020-12-09 14:58:56 +0300

    Feature/cron actor v2 (#307)
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>
    
    Co-authored-by: Mikhail Tagirov <dev.mikhail.tagirov@gmail.com>

---
+++ core/vm/actor/builtin/v2/cron/cron_actor.hpp
@@ -0,0 +1,25 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CPP_FILECOIN_CORE_VM_ACTOR_BUILTEIN_V2_CRON_ACTOR_HPP
+#define CPP_FILECOIN_CORE_VM_ACTOR_BUILTEIN_V2_CRON_ACTOR_HPP
+
+#include "vm/actor/actor_method.hpp"
+#include "vm/actor/builtin/v0/cron/cron_actor.hpp"
+
+namespace fc::vm::actor::builtin::v2::cron {
+
+  /**
+   * Cron actor v2 is identical to Cron actor v0
+   */
+  using State = v0::cron::State;
+  using Construct = v0::cron::Construct;
+  using EpochTick = v0::cron::EpochTick;
+
+  extern const ActorExports exports;
+
+}  // namespace fc::vm::actor::builtin::v2::cron
+
+#endif  // CPP_FILECOIN_CORE_VM_ACTOR_V2_CRON_ACTOR_HPP
