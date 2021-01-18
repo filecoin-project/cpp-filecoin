c99ad62e16ae93df6080067e1ef132e3f358c01b
ortyomka <iurin.art@gmail.com>
2021-01-18 14:03:19 +0300

    Fixed pr
    Signed-off-by: ortyomka <iurin.art@gmail.com>

--- core/sector_storage/fetch_handler.hpp
+++ core/sector_storage/fetch_handler.hpp
@@ -3,8 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef CPP_FILECOIN_CORE_SECTOR_STORAGE_FETCH_HANDLER_HPP
-#define CPP_FILECOIN_CORE_SECTOR_STORAGE_FETCH_HANDLER_HPP
+#pragma once
 
 #include "api/rpc/ws.hpp"
 #include "sector_storage/stores/store.hpp"
@@ -14,5 +13,3 @@ namespace fc::sector_storage {
   api::RouteHandler serveHttp(const std::shared_ptr<stores::LocalStore>& local_store);
 
 }  // namespace fc::sector_storage
-
-#endif  // CPP_FILECOIN_CORE_SECTOR_STORAGE_FETCH_HANDLER_HPP
