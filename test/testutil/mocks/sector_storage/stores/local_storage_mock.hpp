a25f9819d6fcbb1519db50c127a97ac7c5792423
Alexey <Alexey.N.Chernyshov@gmail.com>
2021-02-19 09:03:28 +0300

    miner fixes (#338)
    * miner fixes
    
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>
    
    * fix out-of-gas was fatal
    
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>
    
    Co-authored-by: turuslan <turuslan.devbox@gmail.com>

--- test/testutil/mocks/sector_storage/stores/local_storage_mock.hpp
+++ test/testutil/mocks/sector_storage/stores/local_storage_mock.hpp
@@ -13,14 +13,16 @@
 namespace fc::sector_storage::stores {
   class LocalStorageMock : public LocalStorage {
    public:
-    MOCK_METHOD1(getStat, outcome::result<FsStat>(const std::string &path));
+    MOCK_CONST_METHOD1(getStat,
+                       outcome::result<FsStat>(const std::string &path));
 
-    MOCK_METHOD0(getStorage, outcome::result<StorageConfig>());
+    MOCK_CONST_METHOD0(getStorage,
+                       outcome::result<boost::optional<StorageConfig>>());
 
     MOCK_METHOD1(setStorage,
                  outcome::result<void>(std::function<void(StorageConfig &)>));
-    MOCK_METHOD1(getDiskUsage,
-                 outcome::result<uint64_t>(const std::string &path));
+    MOCK_CONST_METHOD1(getDiskUsage,
+                       outcome::result<uint64_t>(const std::string &path));
   };
 }  // namespace fc::sector_storage::stores
 
