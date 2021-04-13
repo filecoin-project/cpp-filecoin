31e62aba031e5622cd885bd160f4c415ff2b9902
turuslan <turuslan.devbox@gmail.com>
2021-04-13 15:12:07 +0300

    fix, flush index
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- test/core/storage/car/cids_index_test.cpp
+++ test/core/storage/car/cids_index_test.cpp
@@ -5,6 +5,9 @@
 
 #include "storage/car/cids_index/util.hpp"
 
+#include <future>
+
+#include "common/io_thread.hpp"
 #include "primitives/cid/cid_of_cbor.hpp"
 #include "testutil/outcome.hpp"
 #include "testutil/resources/resources.hpp"
@@ -33,6 +36,8 @@ namespace fc::storage::cids_index {
       return loadOrCreateWithProgress(
           car_path, writable, boost::none, nullptr, nullptr);
     }
+
+    void testFlush(std::shared_ptr<boost::asio::io_context> io);
   };
 
   TEST_F(CidsIndexTest, Test) {
@@ -87,5 +92,43 @@ namespace fc::storage::cids_index {
     // index is merged
     EXPECT_OUTCOME_TRUE_1(ipld->setCbor(value2));
     ipld = *load(true);
+  }
+
+  TEST_F(CidsIndexTest, FlushOn) {
+    ipld = *load(true);
+    ipld->flush_on = 3;
+    ipld->setCbor(1).value();
+    EXPECT_EQ(ipld->written.size(), 1);
+    ipld->setCbor(2).value();
+    EXPECT_EQ(ipld->written.size(), 2);
+    ipld->setCbor(3).value();
+    EXPECT_EQ(ipld->written.size(), 0);
+  }
+
+  void CidsIndexTest::testFlush(std::shared_ptr<boost::asio::io_context> io) {
+    ipld = *load(true);
+    ipld->io = io;
+    ipld->flush_on = 5;
+    std::vector<CID> cids;
+    for (auto i{0}; i < 40; ++i) {
+      ipld->setCbor(i).value();
+    }
+    if (io) {
+      std::promise<void> wait;
+      io->post([&] { wait.set_value(); });
+      wait.get_future().get();
+    }
+    for (auto i{0}; i < (int)cids.size(); ++i) {
+      EXPECT_OUTCOME_EQ(ipld->getCbor<int>(cids[i]), i);
+    }
+  }
+
+  TEST_F(CidsIndexTest, FlushSync) {
+    testFlush(nullptr);
+  }
+
+  TEST_F(CidsIndexTest, FlushAsync) {
+    IoThread thread;
+    testFlush(thread.io);
   }
 }  // namespace fc::storage::cids_index
