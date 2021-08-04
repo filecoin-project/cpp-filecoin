1fd0007f7f494ef71f311ed6014bf2be8de0500a
Mikhail Tagirov <dev.mikhail.tagirov@gmail.com>
2021-08-04 13:37:53 +0300

    cleanup
    Signed-off-by: Mikhail Tagirov <dev.mikhail.tagirov@gmail.com>

--- core/vm/actor/builtin/states/impl/bind_states.cpp
+++
@@ -1,7 +0,0 @@
-/**
- * Copyright Soramitsu Co., Ltd. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include "vm/actor/builtin/states/all_states.hpp"
-#include "vm/actor/builtin/types/type_manager/universal_impl.hpp"
