fc6ee8f6c83e17ba1efd12bdb25b17374e6107a7
Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>
2021-09-15 14:31:46 +0300

    sha256 pr fix
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>

--- core/crypto/sha/sha256.hpp
+++ core/crypto/sha/sha256.hpp
@@ -10,5 +10,7 @@
 namespace fc::crypto::sha {
   using Hash256 = std::array<uint8_t, 32u>;
 
-  Hash256 sha256(gsl::span<const uint8_t> input);
+  inline Hash256 sha256(gsl::span<const uint8_t> input) {
+    return libp2p::crypto::sha256(input).value();
+  }
 }  // namespace fc::crypto::sha
