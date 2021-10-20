74ecb082f5ee76cbb116197b76ef92bbc1159e5d
Mikhail Tagirov <dev.mikhail.tagirov@gmail.com>
2021-10-20 16:29:04 +0300

    VerifyDealsForActivation method
    Signed-off-by: Mikhail Tagirov <dev.mikhail.tagirov@gmail.com>

--- core/vm/actor/builtin/v3/market/market_actor.cpp
+++ core/vm/actor/builtin/v3/market/market_actor.cpp
@@ -5,11 +5,44 @@
 
 #include "vm/actor/builtin/v3/market/market_actor.hpp"
 
+#include "vm/actor/builtin/states/market/market_actor_state.hpp"
+#include "vm/toolchain/toolchain.hpp"
+
 namespace fc::vm::actor::builtin::v3::market {
+  using states::MarketActorStatePtr;
+  using toolchain::Toolchain;
 
   ACTOR_METHOD_IMPL(VerifyDealsForActivation) {
-    // todo
-    return Result{{{0, 0, 0}}};
+    const auto address_matcher =
+        Toolchain::createAddressMatcher(runtime.getActorVersion());
+    OUTCOME_TRY(runtime.validateImmediateCallerType(
+        address_matcher->getStorageMinerCodeId()));
+
+    REQUIRE_NO_ERROR_A(state,
+                       runtime.getActorState<MarketActorStatePtr>(),
+                       VMExitCode::kErrIllegalState);
+
+    // Lotus gas conformance
+    REQUIRE_NO_ERROR(state->proposals.amt.loadRoot(),
+                     VMExitCode::kErrIllegalState);
+
+    const auto utils = Toolchain::createMarketUtils(runtime);
+
+    std::vector<SectorWeights> weights;
+    for (const auto &sector : params.sectors) {
+      REQUIRE_NO_ERROR_A(
+          result,
+          utils->validateAndComputeDealWeight(
+              state->proposals, sector.deal_ids, sector.sector_expiry),
+          VMExitCode::kErrIllegalState);
+      const auto &[deal_weight, verified_deal_weight, deal_space] = result;
+
+      weights.push_back({.deal_space = deal_space,
+                         .deal_weight = deal_weight,
+                         .verified_deal_weight = verified_deal_weight});
+    }
+
+    return Result{weights};
   }
 
   const ActorExports exports{
