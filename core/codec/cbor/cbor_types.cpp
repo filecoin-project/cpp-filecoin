a9634fefc9ec621045f0211f678f9e75956e1fb7
Alexey <Alexey.N.Chernyshov@gmail.com>
2021-12-08 17:03:13 +0300

    Feature/retrieval market v1.0.0 (#549)
    Signed-off-by: Alexey Chernyshov <alexey.n.chernyshov@gmail.com>

--- core/codec/cbor/cbor_types.cpp
+++ core/codec/cbor/cbor_types.cpp
@@ -4,84 +4,8 @@
  */
 
 #include "codec/cbor/cbor_codec.hpp"
-#include "markets/retrieval/protocols/retrieval_protocol.hpp"
 #include "primitives/address/address_codec.hpp"
 #include "storage/ipfs/graphsync/extension.hpp"
-
-namespace fc::markets::retrieval {
-  using codec::cbor::CborDecodeStream;
-  using codec::cbor::CborEncodeStream;
-
-  CBOR2_ENCODE(DealProposalParams::Named) {
-    auto m{CborEncodeStream::map()};
-    m["Selector"] << v.selector;
-    m["PieceCID"] << v.piece;
-    m["PricePerByte"] << v.price_per_byte;
-    m["PaymentInterval"] << v.payment_interval;
-    m["PaymentIntervalIncrease"] << v.payment_interval_increase;
-    m["UnsealPrice"] << v.unseal_price;
-    return s << m;
-  }
-  CBOR2_DECODE(DealProposalParams::Named) {
-    auto m{s.map()};
-    CborDecodeStream::named(m, "Selector") >> v.selector;
-    CborDecodeStream::named(m, "PieceCID") >> v.piece;
-    CborDecodeStream::named(m, "PricePerByte") >> v.price_per_byte;
-    CborDecodeStream::named(m, "PaymentInterval") >> v.payment_interval;
-    CborDecodeStream::named(m, "PaymentIntervalIncrease")
-        >> v.payment_interval_increase;
-    CborDecodeStream::named(m, "UnsealPrice") >> v.unseal_price;
-    return s;
-  }
-
-  CBOR2_ENCODE(DealProposal::Named) {
-    auto m{CborEncodeStream::map()};
-    m["PayloadCID"] << v.payload_cid;
-    m["ID"] << v.deal_id;
-    m["Params"] << static_cast<const DealProposalParams::Named &>(v.params);
-    return s << m;
-  }
-  CBOR2_DECODE(DealProposal::Named) {
-    auto m{s.map()};
-    CborDecodeStream::named(m, "PayloadCID") >> v.payload_cid;
-    CborDecodeStream::named(m, "ID") >> v.deal_id;
-    CborDecodeStream::named(m, "Params")
-        >> *(static_cast<DealProposalParams::Named *>(&v.params));
-    return s;
-  }
-
-  CBOR2_ENCODE(DealResponse::Named) {
-    auto m{CborEncodeStream::map()};
-    m["Status"] << v.status;
-    m["ID"] << v.deal_id;
-    m["PaymentOwed"] << v.payment_owed;
-    m["Message"] << v.message;
-    return s << m;
-  }
-  CBOR2_DECODE(DealResponse::Named) {
-    auto m{s.map()};
-    CborDecodeStream::named(m, "Status") >> v.status;
-    CborDecodeStream::named(m, "ID") >> v.deal_id;
-    CborDecodeStream::named(m, "PaymentOwed") >> v.payment_owed;
-    CborDecodeStream::named(m, "Message") >> v.message;
-    return s;
-  }
-
-  CBOR2_ENCODE(DealPayment::Named) {
-    auto m{CborEncodeStream::map()};
-    m["ID"] << v.deal_id;
-    m["PaymentChannel"] << v.payment_channel;
-    m["PaymentVoucher"] << v.payment_voucher;
-    return s << m;
-  }
-  CBOR2_DECODE(DealPayment::Named) {
-    auto m{s.map()};
-    CborDecodeStream::named(m, "ID") >> v.deal_id;
-    CborDecodeStream::named(m, "PaymentChannel") >> v.payment_channel;
-    CborDecodeStream::named(m, "PaymentVoucher") >> v.payment_voucher;
-    return s;
-  }
-}  // namespace fc::markets::retrieval
 
 namespace fc::storage::ipfs::graphsync {
   using codec::cbor::CborDecodeStream;
--- core/markets/retrieval/provider/impl/retrieval_provider_impl.hpp
+++ core/markets/retrieval/provider/impl/retrieval_provider_impl.hpp
@@ -10,6 +10,7 @@
 #include "common/libp2p/cbor_stream.hpp"
 #include "common/logger.hpp"
 #include "data_transfer/dt.hpp"
+#include "markets/common.hpp"
 #include "markets/retrieval/protocols/query_protocol.hpp"
 #include "markets/retrieval/protocols/retrieval_protocol.hpp"
 #include "markets/retrieval/provider/retrieval_provider.hpp"
@@ -22,6 +23,8 @@
 namespace fc::markets::retrieval::provider {
   using common::libp2p::CborStream;
   using data_transfer::DataTransfer;
+  using data_transfer::DataTransferMessage;
+  using data_transfer::DataTransferResponse;
   using data_transfer::PeerDtId;
   using data_transfer::PeerGsId;
   using ::fc::miner::Miner;
@@ -32,19 +35,22 @@ namespace fc::markets::retrieval::provider {
   using ::fc::storage::piece::PieceStorage;
   using libp2p::Host;
   using primitives::BigInt;
+  using primitives::DealId;
   using primitives::SectorNumber;
   using primitives::piece::PieceData;
   using primitives::piece::UnpaddedPieceSize;
   using GsResStatus = ::fc::storage::ipfs::graphsync::ResponseStatusCode;
 
   struct DealState {
-    DealState(PeerDtId pdtid, PeerGsId pgsid, const DealProposal &proposal)
-        : proposal{proposal},
-          state{proposal.params},
+    DealState(PeerDtId pdtid,
+              PeerGsId pgsid,
+              std::shared_ptr<DealProposal> proposal)
+        : proposal{std::move(proposal)},
+          state{this->proposal->params},
           pdtid{std::move(pdtid)},
           pgsid{std::move(pgsid)} {}
 
-    DealProposal proposal;
+    std::shared_ptr<DealProposal> proposal;
     State state;
     PeerDtId pdtid;
     PeerGsId pgsid;
@@ -67,22 +73,8 @@ namespace fc::markets::retrieval::provider {
 
     RetrievalAsk getAsk() const override;
     void setAsk(const RetrievalAsk &ask) override;
-
-    void onProposal(const PeerDtId &pdtid,
-                    const PeerGsId &pgsid,
-                    const DealProposal &proposal);
-    void onPayment(const std::shared_ptr<DealState> &deal,
-                   const DealPayment &payment);
-    void doUnseal(const std::shared_ptr<DealState> &deal);
-    void doBlocks(const std::shared_ptr<DealState> &deal);
-    void doComplete(const std::shared_ptr<DealState> &deal);
-    bool hasOwed(const std::shared_ptr<DealState> &deal);
-    void doFail(const std::shared_ptr<DealState> &deal, std::string error);
-
     void start() override;
-
     void setPricePerByte(TokenAmount amount) override;
-
     void setPaymentInterval(uint64_t payment_interval,
                             uint64_t payment_interval_increase) override;
 
@@ -91,7 +83,31 @@ namespace fc::markets::retrieval::provider {
      * Handle query stream
      * @param stream - cbor stream
      */
-    void handleQuery(const std::shared_ptr<CborStream> &stream);
+    template <typename QueryType, typename ResponseType>
+    void handleQuery(const std::shared_ptr<CborStream> &stream) {
+      stream->read<QueryType>(
+          [self{shared_from_this()}, stream](auto request_res) {
+            if (request_res.has_error()) {
+              self->respondErrorQueryResponse<ResponseType>(
+                  stream, request_res.error().message());
+              return;
+            }
+            auto response_res = self->makeQueryResponse(request_res.value());
+            if (response_res.has_error()) {
+              self->respondErrorQueryResponse<ResponseType>(
+                  stream, response_res.error().message());
+              return;
+            }
+            stream->write(ResponseType{response_res.value()},
+                          [self, stream](auto written) {
+                            if (written.has_error()) {
+                              self->logger_->error("Error while error response "
+                                                   + written.error().message());
+                            }
+                            closeStreamGracefully(stream, self->logger_);
+                          });
+          });
+    }
 
     /**
      * Look for piece by cid and make response
@@ -105,8 +121,155 @@ namespace fc::markets::retrieval::provider {
      * @param stream to send and close
      * @param message error to send
      */
+    template <typename ResponseType>
     void respondErrorQueryResponse(const std::shared_ptr<CborStream> &stream,
-                                   const std::string &message);
+                                   const std::string &message) {
+      ResponseType response;
+      response.response_status = QueryResponseStatus::kQueryResponseError;
+      response.item_status = QueryItemStatus::kQueryItemUnknown;
+      response.message = message;
+      stream->write(response, [self{shared_from_this()}, stream](auto written) {
+        if (written.has_error()) {
+          self->logger_->error("Error while error response "
+                               + written.error().message());
+        }
+        closeStreamGracefully(stream, self->logger_);
+      });
+    }
+
+    /**
+     * Sets datatransfer pull response for DealResponse for particular protocol
+     * @tparam DealResponseType - protocol dependent response type
+     * @param deal_response - response
+     */
+    template <typename DealResponseType>
+    void acceptDataTransferPullResponseType(const PeerDtId &pdtid,
+                                            const PeerGsId &pgsid,
+                                            const DealResponse &deal_response) {
+      datatransfer_->acceptPull(
+          pdtid,
+          pgsid,
+          DealResponseType::type,
+          codec::cbor::encode(DealResponseType{deal_response}).value());
+    }
+
+    /**
+     * Accepts datatransfer pull for DealResponse for all protocol versions.
+     * @param deal - served deal
+     * @param deal_response - response to send
+     */
+    void acceptDataTransferPullResponse(const std::shared_ptr<DealState> &deal,
+                                        const DealResponse &deal_response);
+
+    /**
+     * Post particular protocol-dependent DealResponse.
+     * @tparam DealResponseType - protocol-dependent DealResponse type
+     * @param full_content - is it full or partial response
+     * @param deal_response - response to send
+     */
+    template <typename DealResponseType>
+    void postDataTransferResponseType(const PeerDtId &pdtid,
+                                      const PeerGsId &pgsid,
+                                      bool full_content,
+                                      const DealResponse &deal_response) {
+      datatransfer_->gs->postResponse(
+          pgsid,
+          {full_content ? GsResStatus::RS_FULL_CONTENT
+                        : GsResStatus::RS_PARTIAL_RESPONSE,
+           {DataTransfer::makeExt(DataTransferMessage{DataTransferResponse{
+               full_content ? data_transfer::MessageType::kCompleteMessage
+                            : data_transfer::MessageType::kVoucherResultMessage,
+               true,
+               false,
+               pdtid.id,
+               CborRaw{codec::cbor::encode<DealResponseType>({deal_response})
+                           .value()},
+               DealResponseType::type,
+           }})},
+           {}});
+    }
+
+    /**
+     * Post DealResponse for all protocol versions.
+     * @param deal - current deal
+     * @param full_content - is it full or partial response
+     * @param deal_response - response to post
+     */
+    void postDataTransferResponse(const std::shared_ptr<DealState> &deal,
+                                  bool full_content,
+                                  const DealResponse &deal_response);
+
+    /**
+     * Rejects pull with particular protocol version.
+     * @tparam DealResponseType - protocol dependent response type
+     * @param deal_id - current deal id
+     * @param status - deal status
+     * @param message - response message
+     */
+    template <typename DealResponseType>
+    void rejectDataTransferPullResponseType(const PeerDtId &pdtid,
+                                            const PeerGsId &pgsid,
+                                            DealId deal_id,
+                                            DealStatus status,
+                                            const std::string &message) {
+      datatransfer_->rejectPull(
+          pdtid,
+          pgsid,
+          DealResponseType::type,
+          CborRaw{codec::cbor::encode(
+                      DealResponseType{{status, deal_id, {}, message}})
+                      .value()});
+    }
+
+    /**
+     * Rejects datatransport pull with reason for all protocol version.
+     * @param protocol - proposal protocol version
+     * @param deal_id - current deal id
+     * @param status - deal status
+     * @param message - reject reason
+     */
+    void rejectDataTransferPullResponse(const std::string &protocol,
+                                        const PeerDtId &pdtid,
+                                        const PeerGsId &pgsid,
+                                        primitives::DealId deal_id,
+                                        DealStatus status,
+                                        const std::string &message);
+
+    /**
+     * Sets particular pull-in handler for payments.
+     * @tparam DealPaymentType - protocol dependent type
+     * @param deal - served deal
+     */
+    template <typename DealPaymentType>
+    void acceptDataTransferPaymentType(const std::shared_ptr<DealState> &deal) {
+      datatransfer_->pulling_in.emplace(
+          deal->pdtid, [this, deal](auto &type, auto _voucher) {
+            if (auto _payment{codec::cbor::decode<DealPaymentType>(_voucher)}) {
+              io_.io->post([=] { onPayment(deal, _payment.value()); });
+            } else {
+              doFail(deal, _payment.error().message());
+            }
+          });
+    }
+
+    /**
+     * Sets handler for datatransfer pull-in for payment for all supported
+     * protocol versions.
+     * @param deal - to wait payment for
+     */
+    void acceptDataTransferPayment(const std::shared_ptr<DealState> &deal);
+
+    void onProposal(const PeerDtId &pdtid,
+                    const PeerGsId &pgsid,
+                    const std::shared_ptr<DealProposal> &proposal);
+    void onPayment(const std::shared_ptr<DealState> &deal,
+                   const DealPayment &payment);
+    void doUnseal(const std::shared_ptr<DealState> &deal);
+    void doBlocks(const std::shared_ptr<DealState> &deal);
+    void doComplete(const std::shared_ptr<DealState> &deal);
+    bool hasOwed(const std::shared_ptr<DealState> &deal);
+    void doFail(const std::shared_ptr<DealState> &deal,
+                const std::string &error);
 
     /**
      * Unseal sector
