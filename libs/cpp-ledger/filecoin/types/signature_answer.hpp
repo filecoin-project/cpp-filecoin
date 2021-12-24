1c430b939ee20cb97cc6f163ddc69a0f94bfd603
Mikhail Tagirov <dev.mikhail.tagirov@gmail.com>
2021-12-24 11:43:20 +0300

    filecoin interface
    Signed-off-by: Mikhail Tagirov <dev.mikhail.tagirov@gmail.com>

---
+++ libs/cpp-ledger/filecoin/types/signature_answer.hpp
@@ -0,0 +1,36 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include "cpp-ledger/common/types.hpp"
+
+namespace ledger::filecoin {
+
+  struct SignatureAnswer {
+    Bytes r;
+    Bytes s;
+    uint8_t v{};
+    Bytes derSignature;
+
+    inline bool operator==(const SignatureAnswer &other) const {
+      return r == other.r && s == other.s && v == other.v
+             && derSignature == other.derSignature;
+    }
+
+    inline bool operator!=(const SignatureAnswer &other) const {
+      return !(*this == other);
+    }
+
+    inline Bytes SignatureBytes() const {
+      Bytes out;
+      out.insert(out.end(), r.begin(), r.end());
+      out.insert(out.end(), s.begin(), s.end());
+      out.push_back(v);
+      return out;
+    }
+  };
+
+}  // namespace ledger::filecoin
