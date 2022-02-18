03fd9bd7008d0704d1a5e0b3ea268a0cd886da81
Garrett Brown <garrett.brown@aclima.io>
2022-02-18 00:06:02 -0800

    Cross compiling fixes, improvements and additions (#128)
    Signed-off-by: Garrett Brown <eigendude@gmail.com>

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,9 +1,11 @@
 # hunter dependencies
 # https://docs.hunter.sh/en/latest/packages/
 
-# https://docs.hunter.sh/en/latest/packages/pkg/GTest.html
-hunter_add_package(GTest)
-find_package(GTest CONFIG REQUIRED)
+if (TESTING)
+  # https://docs.hunter.sh/en/latest/packages/pkg/GTest.html
+  hunter_add_package(GTest)
+  find_package(GTest CONFIG REQUIRED)
+endif()
 
 # https://docs.hunter.sh/en/latest/packages/pkg/Boost.html
 hunter_add_package(Boost COMPONENTS date_time filesystem iostreams random program_options thread)
@@ -44,9 +46,6 @@ find_package(leveldb CONFIG REQUIRED)
 # https://github.com/soramitsu/libp2p
 hunter_add_package(libp2p)
 find_package(libp2p CONFIG REQUIRED)
-
-hunter_add_package(c-ares)
-find_package(c-ares CONFIG REQUIRED)
 
 hunter_add_package(soralog)
 find_package(soralog CONFIG REQUIRED)
