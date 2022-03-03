b753f9a01ac8426388c4d5a03b786c668ff39f6c
Markuu-s <qwert11223381@gmail.com>
2022-03-04 02:09:36 +0300

    Fix branch feature/cli_wallet for pr.
    Signed-off-by: Markuu-s <qwert11223381@gmail.com>

--- core/cli/cli.hpp
+++ core/cli/cli.hpp
@@ -10,6 +10,7 @@
 #include <map>
 #include <memory>
 #include <typeindex>
+#include <utility>
 
 #include "cli/try.hpp"
 
@@ -136,4 +137,123 @@ namespace fc::cli {
   using Group = Empty;
 
   struct ShowHelp {};
+
+  class TableWriter {
+    struct Column {
+      std::string name;
+      bool separate_line;
+      size_t lines = 0;
+    };
+    using Row = std::map<size_t, std::string>;
+
+    std::vector<Column> columns;
+    std::vector<Row> rows;
+
+   public:
+    static Column newColumn(const std::string &name) {
+      Column column{.name = name, .separate_line = false};
+      return column;
+    }
+
+    static Column newLineColumn(const std::string &name) {
+      Column column{.name = name, .separate_line = true};
+      return column;
+    }
+
+    explicit TableWriter(std::vector<Column> cols) : columns(std::move(cols)) {}
+
+    void write(std::map<std::string, std::string> range) {
+      std::map<size_t, std::string> by_column_id;
+      bool flag = false;
+
+      while (not flag) {
+        for (auto &[columnName, value] : range) {
+          for (size_t i = 0; i < columns.size(); ++i) {
+            Column &column = columns[i];
+            if (column.name == columnName) {
+              by_column_id[i] = std::move(value);
+              range.erase(columnName);
+              column.lines++;
+              flag = true;
+              break;
+            }
+          }
+
+          if (flag) {
+            break;
+          }
+          by_column_id[columns.size()] = std::move(value);
+          range.erase(columnName);
+          columns.push_back(
+              Column{.name = columnName, .separate_line = false, .lines = 1});
+        }
+        flag = not flag;
+      }
+
+      rows.push_back(by_column_id);
+    }
+
+    void flush() {
+      std::vector<size_t> col_lengths(columns.size());
+
+      std::map<size_t, std::string> header;
+      for (size_t i = 0; i < columns.size(); ++i) {
+        const Column &column = columns[i];
+        if (column.separate_line) {
+          continue;
+        }
+        header[i] = column.name;
+      }
+
+      rows.insert(rows.begin(), header);
+
+      for (size_t i = 0; i < columns.size(); ++i) {
+        const Column &column = columns[i];
+        if (column.lines == 0) {
+          continue;
+        }
+
+        for (auto &row : rows) {
+          if (row.find(i) == row.end()) {
+            continue;
+          }
+          std::string value = row[i];
+          if (value.size() > col_lengths[i]) {
+            col_lengths[i] = value.size();
+          }
+        }
+      }
+
+      for (Row &row : rows) {
+        std::vector<std::string> cols(columns.size());
+
+        for (size_t i = 0; i < columns.size(); ++i) {
+          const Column &column = columns[i];
+          if (column.lines == 0) {
+            continue;
+          }
+
+          std::string for_print = row[i];
+          const size_t pad = col_lengths[i] + 2 - for_print.size();
+          if (not column.separate_line && column.lines > 0) {
+            for_print += std::string(pad, ' ');
+            fmt::print(for_print);
+          }
+
+          cols.push_back(for_print);
+        }
+
+        fmt::print("\n");
+
+        for (size_t i = 0; i < columns.size(); ++i) {
+          const Column &column = columns[i];
+          if (not column.separate_line || cols[i].empty()) {
+            continue;
+          }
+
+          fmt::print("  {}: {}\n", column.name, cols[i]);
+        }
+      }
+    }
+  };
 }  // namespace fc::cli
---
+++ core/cli/node/wallet.hpp
@@ -0,0 +1,379 @@
+/**
+* Copyright Soramitsu Co., Ltd. All Rights Reserved.
+* SPDX-License-Identifier: Apache-2.0
+*/
+
+#pragma once
+
+#include "api/rpc/json.hpp"
+#include "cli/node/node.hpp"
+#include "cli/validate/address.hpp"
+#include "codec/json/json.hpp"
+#include "common/file.hpp"
+#include "common/hexutil.hpp"
+#include "crypto/signature/signature.hpp"
+#include "primitives/big_int.hpp"
+#include "storage/ipfs/api_ipfs_datastore/api_ipfs_datastore.hpp"
+#include "vm/state/impl/state_tree_impl.hpp"
+
+namespace fc::cli::_node {
+ using api::KeyInfo;
+ using api::MarketBalance;
+ using api::MsgWait;
+ using boost::lexical_cast;
+ using common::hex_lower;
+ using common::unhex;
+ using crypto::signature::Signature;
+ using fc::vm::actor::Actor;
+ using primitives::BigInt;
+ using primitives::GasAmount;
+ using primitives::Nonce;
+ using primitives::TokenAmount;
+ using primitives::address::Address;
+ using primitives::address::decodeFromString;
+ using primitives::address::encodeToString;
+ using primitives::tipset::TipsetCPtr;
+ using storage::ipfs::ApiIpfsDatastore;
+ using vm::message::SignedMessage;
+ using vm::message::UnsignedMessage;
+ using vm::state::StateTreeImpl;
+
+ struct walletNew : Empty {
+   CLI_RUN() {
+     const Node::Api api{argm};
+     const std::string type =
+         argv.empty() ? "secp256k1"
+                      : cliArgv<std::string>(
+                          argv, 0, "[bls|secp256k1 (default secp256k1)]");
+
+     const Address address =
+         cliTry(api->WalletNew(type), "Creating new wallet...");
+
+     fmt::print("{}", encodeToString(address));
+   }
+ };
+
+ struct walletList {
+   struct Args {
+     CLI_BOOL("address-only,a", "only print addresses") address_only;
+     CLI_BOOL("id,i", "Output ID addresses") id;
+     CLI_BOOL("market,m", "Output market balances") market;
+
+     CLI_OPTS() {
+       Opts opts;
+       address_only(opts);
+       id(opts);
+       market(opts);
+       return opts;
+     }
+   };
+   CLI_RUN() {
+     const Node::Api api{argm};
+
+     const std::vector<Address> addresses =
+         cliTry(api->WalletList(), "Getting list of wallets...");
+
+     const Address default_address = cliTry(
+         api->WalletDefaultAddress(), "Getting default address of wallet...");
+
+     TableWriter table_writer =
+         TableWriter({TableWriter::newColumn("Address"),
+                      TableWriter::newColumn("ID"),
+                      TableWriter::newColumn("Balance"),
+                      TableWriter::newColumn("Market(Avail)"),
+                      TableWriter::newColumn("Market(Locked)"),
+                      TableWriter::newColumn("Nonce"),
+                      TableWriter::newColumn("Default"),
+                      TableWriter::newColumn("Error")});
+
+     for (const Address &address : addresses) {
+       if (args.address_only) {
+         fmt::print("{}\n", encodeToString(address));
+       } else {
+         auto maybe_actor = api->StateGetActor(address, TipsetKey{});
+
+         if (maybe_actor.has_error()) {
+           table_writer.write({{"Address", encodeToString(address)},
+                               {"Error", "Error get actor"}});
+           continue;
+         }
+         const Actor actor = maybe_actor.value();
+
+         std::map<std::string, std::string> row{
+             {"Address", encodeToString(address)},
+             {"Balance", lexical_cast<std::string>(actor.balance)},
+             {"Nonce", std::to_string(actor.nonce)}};
+
+         if (address == default_address) {
+           row["Default"] = "X";
+         }
+
+         if (args.id) {
+           auto maybe_id = api->StateLookupID(address, TipsetKey{});
+           if (maybe_id.has_error()) {
+             row["ID"] = "n/a";
+           } else {
+             row["ID"] = encodeToString(maybe_id.value());
+           }
+         }
+
+         if (args.market) {
+           const auto maybe_balance = api->StateMarketBalance(address, TipsetKey{});
+           if (maybe_balance.has_value()) {
+             const MarketBalance balance = maybe_balance.value();
+             row["Market(Avail)"] =
+                 lexical_cast<std::string>(balance.escrow - balance.locked);
+             row["Market(Locked)"] = lexical_cast<std::string>(balance.locked);
+           }
+         }
+
+         table_writer.write(row);
+       }
+     }
+
+     if (not args.address_only) {
+       table_writer.flush();
+     }
+   }
+ };
+
+ struct walletBalance : Empty {
+   CLI_RUN() {
+     const Node::Api api{argm};
+     const Address address = (argv.size() == 1)
+                                 ? cliArgv<Address>(argv, 0, "address")
+                                 : cliTry(api->WalletDefaultAddress(),
+                                          "Getting default address...");
+
+     const TokenAmount balance =
+         cliTry(api->WalletBalance(address), "Getting balance of wallet...");
+
+     if (balance == 0) {
+       fmt::print("{} (warning: may display 0 if chain sync in progress)\n",
+                  balance);
+     } else {
+       fmt::print("{}\n", balance);
+     }
+   }
+ };
+
+ struct walletAddBalance {
+   struct Args {
+     CLI_OPTIONAL("from,f", "Address from take balance", Address) from;
+     CLI_DEFAULT("gas-limit", "Limit of gas", GasAmount, {0}) gas_limit;
+
+     CLI_OPTS() {
+       Opts opts;
+       from(opts);
+       gas_limit(opts);
+       return opts;
+     }
+   };
+   CLI_RUN() {
+     const Node::Api api{argm};
+
+     const Address address_from = (args.from)
+                                      ? *args.from
+                                      : cliTry(api->WalletDefaultAddress(),
+                                               "Getting default address...");
+     const Address address_to{
+         cliArgv<Address>(argv, 0, "Address to add balance")};
+     const TokenAmount amount{
+         cliArgv<TokenAmount>(argv, 1, "Amount of add balance")};
+
+     const SignedMessage signed_message = cliTry(api->MpoolPushMessage(
+         vm::message::UnsignedMessage(
+             address_to, address_from, 0, amount, 0, *args.gas_limit, 0, {}),
+         boost::none));
+
+     const MsgWait message_wait =
+         cliTry(api->StateWaitMsg(signed_message.getCid(), 1, 10, false),
+                "Wait message");
+   }
+ };
+
+ struct walletDefault : Empty {
+   CLI_RUN() {
+     const Node::Api api{argm};
+     const Address default_address =
+         cliTry(api->WalletDefaultAddress(), "Getting default address...");
+     fmt::print("{}", encodeToString(default_address));
+   }
+ };
+
+ struct walletSetDefault : Empty {
+   CLI_RUN() {
+     const Node::Api api{argm};
+     const Address address{
+         cliArgv<Address>(argv, 0, "Address for set as default")};
+
+     cliTry(api->WalletSetDefault(address), "Setting default address");
+   }
+ };
+
+ struct walletImport {
+   struct Args {
+     CLI_DEFAULT("format,f",
+                 "specify input format for key [hex-lotus|json-lotus]",
+                 std::string,
+                 {"hex-lotus"})
+     format;
+     CLI_BOOL("as-default", "import the given key as your new default key")
+     as_default;
+     CLI_OPTS() {
+       Opts opts;
+       format(opts);
+       as_default(opts);
+       return opts;
+     }
+   };
+   CLI_RUN() {
+     const Node::Api api{argm};
+
+     if (*args.format != "hex-lotus" && *args.format != "json-lotus") {
+       throw CliError("unrecognized or unsupported format: " + *args.format);
+     }
+
+     const std::string path =
+         (argv.size() == 1) ? cliArgv<std::string>(
+             argv, 0, "<path> (optional, will read from stdin if omitted)")
+                            : "";
+
+     Bytes input_data;
+
+     if (path.empty()) {
+       fmt::print("Enter private key: ");
+       std::string private_key;
+       std::cin >> private_key;
+
+       input_data = Bytes(private_key.begin(), private_key.end());
+     } else {
+       input_data = cliTry(common::readFile(boost::filesystem::path(path)),
+                           "Reading file...");
+     }
+
+     api::KeyInfo key_info;
+
+     if (*args.format == "hex-lotus") {
+       while (*--input_data.end() == '\n') {
+         input_data.pop_back();
+       }
+       input_data =
+           cliTry(unhex(std::string(input_data.begin(), input_data.end())),
+                  "Unhex data...");
+     }
+
+     const auto json =
+         cliTry(codec::json::parse(input_data), "Parse json data...");
+     key_info = cliTry(api::decode<KeyInfo>(json), "Decoding json...");
+
+     const Address address =
+         cliTry(api->WalletImport(key_info), "Importing key...");
+
+     if (args.as_default) {
+       cliTry(api->WalletSetDefault(address), "Set-default...");
+     }
+
+     fmt::print("Imported key {} successfully.\n", encodeToString(address));
+   }
+ };
+
+ struct walletSign : Empty {
+   CLI_RUN() {
+     const Node::Api api{argm};
+     const Address signing_address{
+         cliArgv<Address>(argv, 0, "Signing address")};
+     const std::string hex_message{
+         cliArgv<std::string>(argv, 1, "Hex message")};
+
+     const Bytes decode_message =
+         cliTry(unhex(hex_message), "Decoding hex message...");
+     const Signature signature =
+         cliTry(api->WalletSign(signing_address, decode_message),
+                "Signing message...");
+
+     fmt::print("{}\n", hex_lower(signature.toBytes()));
+   }
+ };
+
+ struct walletVerify : Empty {
+   CLI_RUN() {
+     const Node::Api api{argm};
+     const Address signing_address{
+         cliArgv<Address>(argv, 0, "Signing address")};
+     const std::string hex_message{
+         cliArgv<std::string>(argv, 1, "Hex message")};
+     const std::string signature{cliArgv<std::string>(argv, 2, "Signature")};
+
+     const Bytes decode_message =
+         cliTry(unhex(hex_message), "Decoding message...");
+
+     const Bytes signing_bytes =
+         cliTry(unhex(signature), "Decoding signature...");
+
+     const Signature signature_from_bytes =
+         cliTry(Signature::fromBytes(signing_bytes),
+                "Getting signature from bytes...");
+     bool flagOk = cliTry(api->WalletVerify(
+         signing_address, decode_message, signature_from_bytes));
+     if (flagOk) {
+       fmt::print("valid\n");
+     } else {
+       fmt::print("invalid\nCLI Verify called with invalid signature\n");
+     }
+   }
+ };
+
+ struct walletDelete : Empty {
+   CLI_RUN() {
+     const Node::Api api{argm};
+     const Address address{cliArgv<Address>(argv, 0, "Address for delete")};
+
+     cliTry(api->WalletDelete(address), "Deleting address...");
+   }
+ };
+
+ struct walletMarketAdd {
+   struct Args {
+     CLI_OPTIONAL("from,f",
+                  "Specify address to move funds from, otherwise it will use "
+                  "the default wallet address",
+                  Address)
+     from;
+     CLI_OPTIONAL("address,a",
+                  "Market address to move funds to (account or miner actor "
+                  "address, defaults to --from address)",
+                  Address)
+     address;
+
+     CLI_OPTS() {
+       Opts opts;
+       from(opts);
+       address(opts);
+       return opts;
+     }
+   };
+   CLI_RUN() {
+     const Node::Api api{argm};
+     const TokenAmount amt{cliArgv<TokenAmount>(argv, 0, "Amount")};
+     const Address address_from =
+         (args.from ? (*args.from)
+                    : cliTry(api->WalletDefaultAddress(),
+                             "Getting default address..."));
+
+     const Address address = (args.address) ? *args.address : address_from;
+
+     fmt::print(
+         "Submitting Add Balance message for amount {} for address {}\n",
+         amt,
+         encodeToString(address));
+
+     const auto cid_signed_message = cliTry(
+         api->MarketAddBalance(address_from, address, amt), "Add balance...");
+     const MsgWait message_wait = cliTry(
+         api->StateWaitMsg(cid_signed_message, 1, 10, false), "Wait message");
+
+     fmt::print("Add balance message cid : {}\n", cid_signed_message);
+   }
+ };
+}  // namespace fc::cli::_node
